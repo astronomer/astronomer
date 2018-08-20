cd48006146dcea2918aadff7c17b88e634956d58
schnie <greg@astronomer.io>
2018-08-20 08:41:04 -0400

    Fix linting issues

--- docker/platform/install/include/install.sh
+++ docker/platform/install/include/install.sh
@@ -2,10 +2,10 @@
 
 TAG=$1
 
-if (( $EUID != 0 )); then
+if (( EUID != 0 )); then
     echo "Please run command as root."
     exit
 fi
 
 DOWNLOADER="https://raw.githubusercontent.com/astronomerio/astro-cli/master/godownloader.sh"
-curl -sL -o- ${DOWNLOADER} | bash -s -- -b /usr/local/bin $TAG
+curl -sL -o- "${DOWNLOADER}" | bash -s -- -b /usr/local/bin "$TAG"
