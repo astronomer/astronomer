89716224040a8122e6b0c152754675c8c652307e
sjmiller609 <sjmiller609@gmail.com>
2020-02-07 14:42:24 -0500

    Test k8s versions 1.12 to 1.17 (#542)
    * Speed up local CI runs
    
    * Test on multiple Kube versions
    
    * Fix formatting
    
    * Fix CI workflow
    
    * Add apiVersion to all subcharts
    
    * Test K8s versions 1.12 to 1.17
    
    * Fix linting errors and 1.12-1.17 kube
    
    * Update subchart versions
    
    * Retry kind create cluster once
    
    * Decrease timeout to 400 seconds
    
    * Always get the debugging output
    
    * Debug typo
    
    * Update .circleci/config.yml
    
    Co-Authored-By: Greg Neiheisel <greg@astronomer.io>
    
    * Enable liniting without duplicating global values
    
    Co-authored-by: Greg Neiheisel <greg@astronomer.io>

---
+++ .circleci/generate_circleci_config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+This script is used to create the circle config file
+so that we can stay DRY.
+"""
+
+import os
+from jinja2 import Template
+
+# When adding a new version, look up the most
+# recent patch version on Dockerhub
+KUBE_VERSIONS = ['1.12.10',
+                 '1.13.12',
+                 '1.14.10',
+                 '1.15.7',
+                 '1.16.4',
+                 '1.17.0']
+
+def main():
+    """ Render the Jinja2 template file
+    """
+    circle_directory = os.path.dirname(os.path.realpath(__file__))
+    config_template_path = os.path.join(circle_directory, "config.yml.j2")
+    config_path = os.path.join(circle_directory, "config.yml")
+
+    with open(config_template_path, "r") as circle_ci_config_template:
+        templated_file_content = circle_ci_config_template.read()
+    template = Template(templated_file_content)
+    config = template.render(
+        kube_versions=KUBE_VERSIONS
+    )
+    warning_header = "# Warning: automatically generated file\n" + \
+                     "# Please edit config.yml.j2, and use the script generate_circleci_config.py\n"
+    with open(config_path, "w") as circle_ci_config_file:
+        circle_ci_config_file.write(warning_header)
+        circle_ci_config_file.write(config)
+
+
+if __name__ == "__main__":
+    main()
