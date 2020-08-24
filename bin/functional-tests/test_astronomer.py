afedf48797cdd560160ff3d832f8204a7f2ab492
Steven Miller <sjmiller609@gmail.com>
2020-08-24 10:59:22 -0400

    Add monitoring for kube DNS

---
+++ bin/functional-tests/test_astronomer.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+This file is for system testing the Astronomer Helm chart.
+
+Testinfra is used to create test fixures.
+
+testinfra simplifies and provides syntactic sugar for doing
+execs into a running pods.
+"""
+
+import json
+import os
+import pytest
+import testinfra
+import docker
+
+def test_prometheus_user(prometheus):
+    """ Ensure Airflow is in PATH
+    """
+    user = prometheus.check_output('whoami')
+    assert user == "nobody", \
+        f"Expected prometheus to be running as 'nobody', not '{user}'"
+
+def test_prometheus_targets(prometheus):
+    """ Ensure Airflow is in PATH
+    """
+    data = prometheus.check_output("wget -qO- http://localhost:9090/api/v1/targets")
+    targets = json.loads(data)['data']['activeTargets']
+    for target in targets:
+        assert target['health'] == 'up', \
+            'Expected all prometheus targets to be up. ' + \
+            'Please check the "targets" view in the Prometheus UI'
+
+# Create a test fixture for the promtheus pod
+@pytest.fixture(scope='session')
+def prometheus(request):
+    """ This is the host fixture for testinfra. To read more, please see
+    the testinfra documentation:
+    https://testinfra.readthedocs.io/en/latest/examples.html#test-docker-images
+    """
+    namespace = os.environ.get('NAMESPACE')
+    release_name = os.environ.get('RELEASE_NAME')
+    if not namespace:
+        print("NAMESPACE env var is not present, using 'default' namespace")
+        namespace = 'default'
+    if not release_name:
+        print("RELEASE_NAME env var is not present, assuming 'astronomer' is the release name")
+        release_name = 'astronomer'
+    pod = f'{release_name}-prometheus-0'
+    yield testinfra.get_host(f'kubectl://{pod}?container=prometheus&namespace={namespace}')
+
+
+@pytest.fixture(scope='session')
+def docker_client(request):
+    """ This is a text fixture for the docker client,
+    should it be needed in a test
+    """
+    client = docker.from_env()
+    yield client
+    client.close()
