b1cbbc197c0f2ee773d1f3e2680985c80239f68b
Steven Miller <sjmiller609@gmail.com>
2020-08-26 14:14:27 -0400

    Add houston api test fixture

--- bin/functional-tests/test_astronomer.py
+++ bin/functional-tests/test_astronomer.py
@@ -13,6 +13,21 @@ import os
 import pytest
 import testinfra
 import docker
+from kubernetes import client, config
+
+def create_kube_client(in_cluster=False):
+    """
+    Load and store authentication and cluster information from kube-config
+    file; if running inside a pod, use Kubernetes service account. Use that to
+    instantiate Kubernetes client.
+    """
+    if in_cluster:
+        print("Using in cluster kubernetes configuration")
+        config.load_incluster_config()
+    else:
+        print("Using kubectl kubernetes configuration")
+        config.load_kube_config()
+    return client.CoreV1Api()
 
 def test_prometheus_user(prometheus):
     """ Ensure user is 'nobody'
@@ -20,6 +35,9 @@ def test_prometheus_user(prometheus):
     user = prometheus.check_output('whoami')
     assert user == "nobody", \
         f"Expected prometheus to be running as 'nobody', not '{user}'"
+
+def test_houston_test_fixture(houston_api):
+    data = houston_api.check_output("whoami")
 
 def test_prometheus_targets(prometheus):
     """ Ensure all Prometheus targets are healthy
@@ -30,6 +48,28 @@ def test_prometheus_targets(prometheus):
         assert target['health'] == 'up', \
             'Expected all prometheus targets to be up. ' + \
             'Please check the "targets" view in the Prometheus UI'
+
+# Create a test fixture for the prometheus pod
+@pytest.fixture(scope='session')
+def houston_api(request):
+    """ This is the host fixture for testinfra. To read more, please see
+    the testinfra documentation:
+    https://testinfra.readthedocs.io/en/latest/examples.html#test-docker-images
+    """
+    namespace = os.environ.get('NAMESPACE')
+    release_name = os.environ.get('RELEASE_NAME')
+    if not namespace:
+        print("NAMESPACE env var is not present, using 'default' namespace")
+        namespace = 'default'
+    if not release_name:
+        print("RELEASE_NAME env var is not present, assuming 'astronomer' is the release name")
+        release_name = 'astronomer'
+    kube = create_kube_client()
+    pods = kube.list_namespaced_pod(namespace, label_selector=f"component=houston")
+    pods = pods.items
+    assert len(pods) > 0, "Expected to find at least one pod with label 'component: houston'"
+    pod = pods[0]
+    yield testinfra.get_host(f'kubectl://{pod.metadata.name}?container=houston&namespace={namespace}')
 
 # Create a test fixture for the prometheus pod
 @pytest.fixture(scope='session')
