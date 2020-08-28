02ba7b8a327a29ac524d6e1fab3ed592a98a7425
Steven Miller <sjmiller609@gmail.com>
2020-08-28 10:14:56 -0400

    Collect metrics from houston (#825)

--- bin/functional-tests/test_astronomer.py
+++ bin/functional-tests/test_astronomer.py
@@ -29,7 +29,16 @@ def test_prometheus_targets(prometheus):
     for target in targets:
         assert target['health'] == 'up', \
             'Expected all prometheus targets to be up. ' + \
-            'Please check the "targets" view in the Prometheus UI'
+            'Please check the "targets" view in the Prometheus UI' + \
+            f" Target data from the one that is not up:\n\n{target}"
+
+def test_houston_metrics_are_collected(prometheus):
+    """ Ensure Houston metrics are collected and prefixed with 'houston_'
+    """
+    data = prometheus.check_output("wget -qO- http://localhost:9090/api/v1/query?query=houston_up")
+    parsed = json.loads(data)
+    assert len(parsed['data']['result']) > 0, \
+        f"Expected to find a metric houston_up, but we got this response:\n\n{parsed}"
 
 # Create a test fixture for the prometheus pod
 @pytest.fixture(scope='session')
