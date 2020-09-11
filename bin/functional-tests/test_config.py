e9144e2e5e65c62ac1ada8ecd0b1dbfc7964b04b
Steven Miller <sjmiller609@gmail.com>
2020-09-11 09:44:32 -0400

    Add patch version upgrade testing (#845) (#846)
    * Reorganized functional testing
    
    * Test that the chart version is not already published in order to enforce immutable tags
    
    * Minor fix to make 'helm upgrade' work seamlessly
    
    * Set up downgrade and upgrade testing
    
    * verbose test run
    
    * Add astronomer-interal git repo
    
    * Fix version string
    
    * Set version to 0.20.0
    
    * Disable existing e2e testing
    
    * Speed up CI by changing the DAG structure
    
    * Give prometheus enough time to collect metrics
    
    * Update bin/functional-tests/README.md
    
    Co-authored-by: Daniel <daniel.hoherd@gmail.com>
    
    * Add syntax highlighting to readme
    
    * Add black to precommit
    
    Co-authored-by: Daniel <daniel.hoherd@gmail.com>
    
    Co-authored-by: Daniel <daniel.hoherd@gmail.com>

---
+++ bin/functional-tests/test_config.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+This file is for system testing the Astronomer Helm chart.
+
+Testinfra is used to create test fixures.
+
+testinfra simplifies and provides syntactic sugar for doing
+execs into a running pods.
+"""
+
+import json
+import os
+import docker
+
+
+def test_prometheus_user(prometheus):
+    """Ensure user is 'nobody'"""
+    user = prometheus.check_output("whoami")
+    assert (
+        user == "nobody"
+    ), f"Expected prometheus to be running as 'nobody', not '{user}'"
+
+
+def test_prometheus_targets(prometheus):
+    """Ensure all Prometheus targets are healthy"""
+    data = prometheus.check_output("wget -qO- http://localhost:9090/api/v1/targets")
+    targets = json.loads(data)["data"]["activeTargets"]
+    for target in targets:
+        assert target["health"] == "up", (
+            "Expected all prometheus targets to be up. "
+            + 'Please check the "targets" view in the Prometheus UI'
+            + f" Target data from the one that is not up:\n\n{target}"
+        )
+
+
+def test_core_dns_metrics_are_collected(prometheus):
+    """Ensure CoreDNS metrics are collected.
+
+    This test should work in CI and locally because Kind uses CoreDNS
+    """
+    data = prometheus.check_output(
+        "wget -qO- http://localhost:9090/api/v1/query?query=coredns_dns_request_count_total"
+    )
+    parsed = json.loads(data)
+    assert (
+        len(parsed["data"]["result"]) > 0
+    ), f"Expected to find a metric coredns_dns_request_count_total, but we got this response:\n\n{parsed}"
+
+
+def test_houston_metrics_are_collected(prometheus):
+    """Ensure Houston metrics are collected and prefixed with 'houston_'"""
+    data = prometheus.check_output(
+        "wget -qO- http://localhost:9090/api/v1/query?query=houston_up"
+    )
+    parsed = json.loads(data)
+    assert (
+        len(parsed["data"]["result"]) > 0
+    ), f"Expected to find a metric houston_up, but we got this response:\n\n{parsed}"
