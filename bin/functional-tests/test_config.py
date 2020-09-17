97b46cb7e11bd5a3a3d7cb6e82c94846aa3a233c
Steven Miller <sjmiller609@gmail.com>
2020-09-17 13:22:58 -0400

    Test connection from ingress to default backend

--- bin/functional-tests/test_config.py
+++ bin/functional-tests/test_config.py
@@ -33,7 +33,10 @@ def test_houston_config(houston_api):
             f"Expected not to find 'localhost' in the 'servers' configuration. Found:\n\n{houston_config['nats']}"
 
 def test_houston_can_reach_prometheus(houston_api):
-    houston_api.check_output("wget --timeout=1 http://astronomer-prometheus.astronomer.svc.cluster.local:9090/targets")
+    houston_api.check_output("wget -qO- --timeout=1 http://astronomer-prometheus.astronomer.svc.cluster.local:9090/targets")
+
+def test_nginx_can_reach_default_backend(nginx):
+    nginx.check_output("curl --max-time 1 http://astronomer-nginx-default-backend:8080")
 
 def test_prometheus_targets(prometheus):
     """ Ensure all Prometheus targets are healthy
