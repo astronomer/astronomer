6d884a128544b4a7f79b3ce9d9463edb4fec4bb3
Daniel <danielh@astronomer.io>
2021-05-13 16:31:01 -0700

    Replace LTS 16-to-23 upgrade with 23-to-25 (#1094)

--- migrations/lts-23-to-25-tests/test_lts_upgrade.py
+++ migrations/lts-23-to-25-tests/test_lts_upgrade.py
@@ -15,46 +15,50 @@ git_root_dir = Path(git_repo.git.rev_parse("--show-toplevel"))
 
 def test_upgrade():
     """
-    Functional test for the LTS to LTS upgrade (0.16 to 0.23)
+    Functional test for the LTS to LTS upgrade (0.23 to 0.25)
     """
+    print("Checking current chart version")
+
     with open(Path(git_root_dir / "Chart.yaml"), "r") as f:
         astro_chart_dot_yaml = yaml.safe_load(f.read())
     major, minor, patch = semver(astro_chart_dot_yaml["version"]).release
 
-    assert major == 0 and minor == 23, "This test is only applicable for 0.23"
+    assert major == 0 and minor == 25, "This test is only applicable for 0.25"
 
     upgrade_manifest_path = Path(
         git_root_dir
-        / "migrations/scripts/lts-to-lts/0.16-to-0.23/manifests/upgrade-0.16-to-0.23.yaml"
+        / "migrations/scripts/lts-to-lts/0.23-to-0.25/manifests/upgrade-0.23-to-0.25.yaml"
     )
     rollback_manifest_path = Path(
         git_root_dir
-        / "migrations/scripts/lts-to-lts/0.16-to-0.23/manifests/rollback-0.16-to-0.23.yaml"
+        / "migrations/scripts/lts-to-lts/0.23-to-0.25/manifests/rollback-0.23-to-0.25.yaml"
     )
 
     namespace = environ.get("NAMESPACE")
     release_name = environ.get("RELEASE_NAME")
+    print("Checking NAMESPACE")
     if not namespace:
         print("NAMESPACE env var is not present, using 'astronomer' namespace")
         namespace = "astronomer"
+    print("Checking RELEASE_NAME")
     if not release_name:
         print(
             "RELEASE_NAME env var is not present, assuming 'astronomer' is the release name"
         )
         release_name = "astronomer"
 
-    # Get the existing values
+    print("Checking installed version")
     result = check_output(
         f"helm3 history { release_name } -n { namespace } | tail -n 1", shell=True
     ).decode("utf8")
-    assert "0.16" in result
+    assert "0.23" in result
 
     # Rewrite some parts of the k8s manifest with testing-specific configs
     with open(upgrade_manifest_path, "r") as f:
         upgrade_manifest_data = f.read()
     upgrade_manifest_data = upgrade_manifest_data.replace(
-        "image: quay.io/astronomer/lts-016-023-upgrade:latest",
-        "image: lts-016-023-upgrade:latest",
+        "image: quay.io/astronomer/lts-23-to-25-upgrade:latest",
+        "image: lts-23-to-25-upgrade:latest",
     )
     upgrade_manifest_data = upgrade_manifest_data.replace(
         "imagePullPolicy: Always", "imagePullPolicy: Never"
@@ -70,6 +74,7 @@ def test_upgrade():
                     container["env"].append(
                         {"name": "USE_INTERNAL_HELM_REPO", "value": "True"}
                     )
+                    print("Modified test env with USE_INTERNAL_HELM_REPO=True")
                 upgrade_manifest_yaml[i] = doc
             except KeyError:
                 pass
@@ -79,6 +84,7 @@ def test_upgrade():
     modified_upgrade_manifest_path = f"{upgrade_manifest_path}.test.yaml"
     with open(modified_upgrade_manifest_path, "w") as f:
         f.write(upgrade_manifest_data)
+    print("Performing upgrade from 0.23 to 0.25")
     check_output(f"kubectl apply -f {modified_upgrade_manifest_path}", shell=True)
     timeout = 800
     start_time = time()
@@ -107,15 +113,16 @@ def test_upgrade():
             ).decode("utf8")
             print(logs)
             assert False, "Failed to perform upgrade (timeout!), see logs"
+
     result = check_output(
         "helm3 history astronomer -n astronomer | tail -n 1", shell=True
     ).decode("utf8")
-    assert "0.23" in result and "deployed" in result, "Expected upgrade to be performed"
+    assert "0.25" in result and "deployed" in result, "Expected upgrade to be performed"
     with open(rollback_manifest_path, "r") as f:
         rollback_manifest_data = f.read()
     rollback_manifest_data = rollback_manifest_data.replace(
-        "image: quay.io/astronomer/lts-016-023-upgrade:latest",
-        "image: lts-016-023-upgrade:latest",
+        "image: quay.io/astronomer/lts-23-to-25-upgrade:latest",
+        "image: lts-23-to-25-upgrade:latest",
     )
     rollback_manifest_data = rollback_manifest_data.replace(
         "imagePullPolicy: Always", "imagePullPolicy: Never"
@@ -154,5 +161,5 @@ def test_upgrade():
         "helm3 history astronomer -n astronomer | tail -n 1", shell=True
     ).decode("utf8")
     assert (
-        "0.16" in result and "deployed" in result
+        "0.23" in result and "deployed" in result
     ), "Expected rollback to be performed"
