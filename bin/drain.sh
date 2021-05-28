ed970ee137fd27a8527fa547cab5f2a8ad22d1bd
Daniel <danielh@astronomer.io>
2021-05-28 15:51:09 -0700

    Remove cruft. Make pre-commit pass on entire bin dir. (#1113)

--- bin/drain.sh
+++ bin/drain.sh
@@ -1,4 +1,10 @@
-set -xe
-kubectl uncordon kind-worker
-kubectl cordon kind-worker2
-kubectl drain --force --delete-local-data --ignore-daemonsets kind-worker2
+#!/usr/bin/env bash
+
+cat <<EOF
+Run the following commands to drain:
+
+    kubectl uncordon kind-worker
+    kubectl cordon kind-worker2
+    kubectl drain --force --delete-local-data --ignore-daemonsets kind-worker2
+
+EOF
