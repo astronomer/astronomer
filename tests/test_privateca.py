216d61da8bca676071651e7ce691f5c0a4b5e67b
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-08-10 15:59:14 -0700

    Add some more cacert tests

--- tests/test_privateca.py
+++ tests/test_privateca.py
@@ -2,6 +2,7 @@ from tests.helm_template_generator import render_chart
 import jmespath
 import pytest
 from . import supported_k8s_versions
+from subprocess import CalledProcessError
 
 
 @pytest.mark.parametrize(
@@ -18,7 +19,21 @@ class TestDameonset:
             "spec.template.spec.containers[0].name", doc
         )
 
-    def test_privateca_daemonset(self, kube_version):
+    def test_privateca_daemonset_disabled(self, kube_version):
+        with pytest.raises(CalledProcessError):
+            render_chart(
+                kube_version=kube_version,
+                show_only=["templates/trust-private-ca-on-all-nodes/daemonset.yaml"],
+                values={
+                    "global": {
+                        "privateCaCertsAddToHost": {
+                            "enabled": False,
+                        }
+                    }
+                },
+            )
+
+    def test_privateca_daemonset_enabled(self, kube_version):
         docs = render_chart(
             kube_version=kube_version,
             show_only=["templates/trust-private-ca-on-all-nodes/daemonset.yaml"],
@@ -39,7 +54,7 @@ class TestDameonset:
             for item in jmespath.search("spec.template.spec.containers[*].image", doc)
         )
 
-    def test_privateca_daemonset_with_custom_image(self, kube_version):
+    def test_privateca_daemonset_enabled_with_custom_image(self, kube_version):
         docs = render_chart(
             kube_version=kube_version,
             show_only=["templates/trust-private-ca-on-all-nodes/daemonset.yaml"],
@@ -63,3 +78,58 @@ class TestDameonset:
             "snarks:boojums" in item
             for item in jmespath.search("spec.template.spec.containers[*].image", doc)
         )
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestPSP:
+    def test_privateca_psp_enabled_cacertaddtohost_disabled(self, kube_version):
+        with pytest.raises(CalledProcessError):
+            render_chart(
+                kube_version=kube_version,
+                show_only=["templates/trust-private-ca-on-all-nodes/psp.yaml"],
+                values={
+                    "global": {
+                        "pspEnabled": True,
+                        "privateCaCertsAddToHost": {
+                            "enabled": False,
+                        },
+                    }
+                },
+            )
+
+    def test_privateca_psp_disabled_cacertaddtohost_enabled(self, kube_version):
+        with pytest.raises(CalledProcessError):
+            render_chart(
+                kube_version=kube_version,
+                show_only=["templates/trust-private-ca-on-all-nodes/psp.yaml"],
+                values={
+                    "global": {
+                        "pspEnabled": False,
+                        "privateCaCertsAddToHost": {
+                            "enabled": True,
+                        },
+                    }
+                },
+            )
+
+    def test_privateca_psp_enabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=["templates/trust-private-ca-on-all-nodes/psp.yaml"],
+            values={
+                "global": {
+                    "pspEnabled": True,
+                    "privateCaCertsAddToHost": {
+                        "enabled": True,
+                    },
+                }
+            },
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        assert "PodSecurityPolicy" == jmespath.search("kind", doc)
+        assert "RELEASE-NAME-private-ca" == jmespath.search("metadata.name", doc)
