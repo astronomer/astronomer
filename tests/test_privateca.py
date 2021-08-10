6614fd11a8e5b171e1e36a3663e9cdcd48a0b8ba
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-08-09 18:56:10 -0700

    Allow customization of cert-copy image

---
+++ tests/test_privateca.py
@@ -0,0 +1,65 @@
+from tests.helm_template_generator import render_chart
+import jmespath
+import pytest
+from . import supported_k8s_versions
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestDameonset:
+    @staticmethod
+    def common_tests_daemonset(doc):
+        """Test things common to all daemonsets"""
+        assert "DaemonSet" == jmespath.search("kind", doc)
+        assert "RELEASE-NAME-private-ca" == jmespath.search("metadata.name", doc)
+        assert "cert-copy" == jmespath.search(
+            "spec.template.spec.containers[0].name", doc
+        )
+
+    def test_privateca_daemonset(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=["templates/trust-private-ca-on-all-nodes/daemonset.yaml"],
+            values={
+                "global": {
+                    "privateCaCertsAddToHost": {
+                        "enabled": True,
+                    }
+                }
+            },
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        self.common_tests_daemonset(doc)
+        assert any(
+            "alpine:3.14" in item
+            for item in jmespath.search("spec.template.spec.containers[*].image", doc)
+        )
+
+    def test_privateca_daemonset_with_custom_image(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=["templates/trust-private-ca-on-all-nodes/daemonset.yaml"],
+            values={
+                "global": {
+                    "privateCaCertsAddToHost": {
+                        "enabled": True,
+                        "certCopier": {
+                            "repository": "snarks",
+                            "tag": "boojums",
+                        },
+                    }
+                }
+            },
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        self.common_tests_daemonset(doc)
+        assert any(
+            "snarks:boojums" in item
+            for item in jmespath.search("spec.template.spec.containers[*].image", doc)
+        )
