59472421707736033b567732c61ac656f7749c20
vishnurampg <vishnu@astronomer.io>
2021-08-19 23:15:21 +0530

    test case fixture for global storage class

--- tests/test_global_storageclass.py
+++ tests/test_global_storageclass.py
@@ -8,19 +8,22 @@ chart_files = [
     "charts/prometheus/templates/prometheus-statefulset.yaml",
     "charts/astronomer/templates/registry/registry-statefulset.yaml",
 ]
+doc_ids = [x for x in range(0, len(chart_files))]
 
 
 @pytest.mark.parametrize(
-    "supported_types, expected_output", [("-", ""), ("vishnure", "vishnure")]
+    "supported_types, expected_output", [("-", ""), ("astrosc", "astrosc")]
 )
-def test_global_storageclass(supported_types, expected_output):
+@pytest.mark.parametrize("ids", doc_ids)
+def test_global_storageclass(supported_types, expected_output, ids):
     """Test globalstorageclass feature of alertmanager statefulset template"""
     docs = render_chart(
         values={"global": {"storageClass": supported_types}},
         show_only=chart_files,
     )
     assert len(docs) == 5
-    doc = docs[0]
+
+    doc = docs[ids]
     assert (
         doc["spec"]["volumeClaimTemplates"][0]["spec"]["storageClassName"]
         == expected_output
