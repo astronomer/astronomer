755a23f34babcb2c7d9a4f095e10298a90d9feaf
Alex Liotta <alex@astronomer.io>
2021-09-07 19:51:06 -0400

    add tests

--- tests/test_astronomer_commander.py
+++ tests/test_astronomer_commander.py
@@ -20,9 +20,7 @@ def test_astronomer_commander_deployment(kube_version):
     assert doc["kind"] == "Deployment"
     assert doc["apiVersion"] == "apps/v1"
     assert doc["metadata"]["name"] == "RELEASE-NAME-commander"
-    assert "quay.io/astronomer/ap-commander:0.25.2" in jmespath.search(
-        "spec.template.spec.containers[*].image", doc
-    )
+
     envVarNames = jmespath.search("spec.template.spec.containers[0].env[*].name", doc)
     assert "COMMANDER_UPGRADE_TIMEOUT" in envVarNames
     upgradeTimeoutIndex = envVarNames.index("COMMANDER_UPGRADE_TIMEOUT")
@@ -51,9 +49,7 @@ def test_astronomer_commander_deployment_upgrade_timeout(kube_version):
     assert doc["kind"] == "Deployment"
     assert doc["apiVersion"] == "apps/v1"
     assert doc["metadata"]["name"] == "RELEASE-NAME-commander"
-    assert "quay.io/astronomer/ap-commander:0.25.2" in jmespath.search(
-        "spec.template.spec.containers[*].image", doc
-    )
+    
     envVarNames = jmespath.search("spec.template.spec.containers[0].env[*].name", doc)
     assert "COMMANDER_UPGRADE_TIMEOUT" in envVarNames
     upgradeTimeoutIndex = envVarNames.index("COMMANDER_UPGRADE_TIMEOUT")
