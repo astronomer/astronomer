b7305cc59108ca719fe395a7c57b07f1561f21c0
Alexander Liotta <alex@astronomer.io>
2021-09-08 13:29:59 -0400

    add upgrade timeout to helm chart (#1211)
    * add upgrade timeout to helm chart
    
    * remove local dev change
    
    * improve test readability
    
    * fix test

--- tests/test_astronomer_commander.py
+++ tests/test_astronomer_commander.py
@@ -23,3 +23,37 @@ def test_astronomer_commander_deployment(kube_version):
     assert "quay.io/astronomer/ap-commander:0.25.2" in jmespath.search(
         "spec.template.spec.containers[*].image", doc
     )
+    assert len(doc["spec"]["template"]["spec"]["containers"]) == 1
+    env_vars = {
+        x["name"]: x["value"]
+        for x in doc["spec"]["template"]["spec"]["containers"][0]["env"]
+    }
+    assert env_vars["COMMANDER_UPGRADE_TIMEOUT"] == "300"
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+def test_astronomer_commander_deployment_upgrade_timeout(kube_version):
+    """Test that helm renders a good deployment template for astronomer/commander. when upgrade timeout is set"""
+    docs = render_chart(
+        kube_version=kube_version,
+        values={"astronomer": {"commander": {"upgradeTimeout": 600}}},
+        show_only=["charts/astronomer/templates/commander/commander-deployment.yaml"],
+    )
+
+    assert len(docs) == 1
+    doc = docs[0]
+    assert doc["kind"] == "Deployment"
+    assert doc["apiVersion"] == "apps/v1"
+    assert doc["metadata"]["name"] == "RELEASE-NAME-commander"
+    assert "quay.io/astronomer/ap-commander:0.25.2" in jmespath.search(
+        "spec.template.spec.containers[*].image", doc
+    )
+    assert len(doc["spec"]["template"]["spec"]["containers"]) == 1
+    env_vars = {
+        x["name"]: x["value"]
+        for x in doc["spec"]["template"]["spec"]["containers"][0]["env"]
+    }
+    assert env_vars["COMMANDER_UPGRADE_TIMEOUT"] == "600"
