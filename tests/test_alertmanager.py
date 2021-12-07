bdad4f47cbff467b770bff282ed55f270fced92a
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-06 17:03:45 -0800

    Delete tests for features on in 0.23

--- tests/test_alertmanager.py
+++ tests/test_alertmanager.py
@@ -1,9 +1,7 @@
 from tests.helm_template_generator import render_chart
 import jmespath
-import pytest
 
 
-@pytest.mark.skip("TODO: Fix this test")
 def test_alertmanager_defaults():
     """Test that alertmanager chart looks sane with defaults"""
     docs = render_chart(
@@ -15,7 +13,6 @@ def test_alertmanager_defaults():
     assert doc["kind"] == "StatefulSet"
     assert doc["apiVersion"] == "apps/v1"
     assert doc["metadata"]["name"] == "RELEASE-NAME-alertmanager"
-    assert doc["spec"]["template"]["spec"]["securityContext"]["fsGroup"] == 65534
 
     # rfc1918 configs should be absent from default settings
     assert (
@@ -34,33 +31,3 @@ def test_alertmanager_defaults():
         )
         for value in item
     ] == []
-
-
-@pytest.mark.skip("TODO: fix this test")
-def test_alertmanager_rfc1918():
-    """Test rfc1918 features of alertmanager template"""
-    docs = render_chart(
-        values={"alertmanager": {"enableNonRFC1918": True}},
-        show_only=["charts/alertmanager/templates/alertmanager-statefulset.yaml"],
-    )
-    assert len(docs) == 1
-    doc = docs[0]
-
-    assert doc["kind"] == "StatefulSet"
-    assert doc["apiVersion"] == "apps/v1"
-    assert doc["metadata"]["name"] == "RELEASE-NAME-alertmanager"
-    # assert doc["spec"]["template"]["spec"]["securityContext"]["fsGroup"] == 65534 # TODO: fix this test
-
-    assert any(
-        "--cluster.advertise-address=" in arg
-        for args in jmespath.search("spec.template.spec.containers[*].args", doc)
-        for arg in args
-    )
-    assert all(
-        value
-        for item in jmespath.search(
-            "spec.template.spec.containers[*].env[?name == 'POD_IP'].valueFrom.fieldRef.fieldPath",
-            doc,
-        )
-        for value in item
-    )
