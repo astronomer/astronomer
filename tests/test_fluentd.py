512304b03d61fee8db17ffa83b1b6840c458f988
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-06 17:16:21 -0800

    Delete tests for features on in 0.23

--- tests/test_fluentd.py
+++ tests/test_fluentd.py
@@ -1,36 +1,6 @@
 from tests.helm_template_generator import render_chart
 import pytest
 from . import supported_k8s_versions
-import jmespath
-
-
-@pytest.mark.skip("TODO: fix this test")
-@pytest.mark.parametrize(
-    "kube_version",
-    supported_k8s_versions,
-)
-def test_fluentd_daemonset(kube_version):
-    """Test that helm renders a volume mount for private ca certificates for fluentd daemonset when private-ca-certificates are enabled."""
-    docs = render_chart(
-        kube_version=kube_version,
-        values={"global": {"privateCaCerts": ["private-root-ca"]}},
-        show_only=["charts/fluentd/templates/fluentd-daemonset.yaml"],
-    )
-
-    search_result = jmespath.search(
-        "spec.template.spec.containers[*].volumeMounts[?name == 'private-root-ca']",
-        docs[0],
-    )
-    expected_result = [
-        [
-            {
-                "mountPath": "/usr/local/share/ca-certificates/private-root-ca.pem",
-                "name": "private-root-ca",
-                "subPath": "cert.pem",
-            }
-        ]
-    ]
-    assert search_result == expected_result
 
 
 @pytest.mark.parametrize(
