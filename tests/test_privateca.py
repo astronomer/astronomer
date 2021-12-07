2c2381aaed4e67732a80b012d6a6a161203222b9
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-06 17:09:37 -0800

    Delete tests for features on in 0.23

--- tests/test_privateca.py
+++ tests/test_privateca.py
@@ -5,12 +5,11 @@ from . import supported_k8s_versions
 from subprocess import CalledProcessError
 
 
-@pytest.mark.skip("TODO: fix this test")
 @pytest.mark.parametrize(
     "kube_version",
     supported_k8s_versions,
 )
-class TestDaemonset:
+class TestPrivateCADaemonset:
     @staticmethod
     def common_tests_daemonset(doc):
         """Test things common to all daemonsets"""
@@ -77,37 +76,12 @@ class TestDaemonset:
         )
         assert {"cert.pem"} == set(jmespath.search("[*].subPath", volmounts))
 
-    def test_privateca_daemonset_enabled_with_custom_image(self, kube_version):
-        docs = render_chart(
-            kube_version=kube_version,
-            show_only=["templates/trust-private-ca-on-all-nodes/daemonset.yaml"],
-            values={
-                "global": {
-                    "privateCaCertsAddToHost": {
-                        "enabled": True,
-                        "certCopier": {
-                            "repository": "snarks",
-                            "tag": "boojums",
-                        },
-                    }
-                }
-            },
-        )
-
-        assert len(docs) == 1
-        doc = docs[0]
-        self.common_tests_daemonset(doc)
-        assert any(
-            "snarks:boojums" in item
-            for item in jmespath.search("spec.template.spec.containers[*].image", doc)
-        )
-
 
 @pytest.mark.parametrize(
     "kube_version",
     supported_k8s_versions,
 )
-class TestPSP:
+class TestPrivateCAPSP:
     def test_privateca_psp_enabled_cacertaddtohost_disabled(self, kube_version):
         with pytest.raises(CalledProcessError):
             render_chart(
