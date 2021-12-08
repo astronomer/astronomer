3b2ae5bebce02654fc1b43d2a818f5af811a6a84
pgvishnuram <vishnu@astronomer.io>
2021-12-08 13:57:38 +0530

    basic test cases for authsidecar feature

---
+++ tests/test_authsidecar.py
@@ -0,0 +1,100 @@
+from tests.helm_template_generator import render_chart
+import pytest
+from . import supported_k8s_versions
+import jmespath
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestAuthSidecar:
+    def test_authSidecar_alertmanager(self, kube_version):
+        """Test External ElasticSearch with secret passed from config/values.yaml."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"authSidecar": {"enabled": True}}},
+            show_only=[
+                "charts/alertmanager/templates/alertmanager-statefulset.yaml",
+                "charts/alertmanager/templates/alertmanager-auth-sidecar-configmap.yaml",
+                "charts/alertmanager/templates/alertmanager-service.yaml",
+                "charts/alertmanager/templates/alertmanager-networkpolicy.yaml",
+                "charts/alertmanager/templates/ingress.yaml",
+            ],
+        )
+
+        assert len(docs) == 5
+        doc = docs[0]
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-alertmanager"
+        assert "auth-proxy" == doc["spec"]["template"]["spec"]["containers"][1]["name"]
+
+        assert "Service" == jmespath.search("kind", docs[2])
+        assert "RELEASE-NAME-alertmanager" == jmespath.search("metadata.name", docs[2])
+        assert "ClusterIP" == jmespath.search("spec.type", docs[2])
+        assert {
+            "name": "auth-proxy",
+            "protocol": "TCP",
+            "port": 8084,
+        } in jmespath.search("spec.ports", docs[2])
+
+    def test_authSidecar_prometheus(self, kube_version):
+        """Test External ElasticSearch with secret passed from config/values.yaml."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"authSidecar": {"enabled": True}}},
+            show_only=[
+                "charts/prometheus/templates/prometheus-statefulset.yaml",
+                "charts/prometheus/templates/prometheus-auth-sidecar-configmap.yaml",
+                "charts/prometheus/templates/prometheus-service.yaml",
+                "charts/prometheus/templates/prometheus-networkpolicy.yaml",
+                "charts/prometheus/templates/ingress.yaml",
+            ],
+        )
+
+        assert len(docs) == 5
+        doc = docs[0]
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-prometheus"
+        assert "auth-proxy" == doc["spec"]["template"]["spec"]["containers"][0]["name"]
+
+        assert "Service" == jmespath.search("kind", docs[2])
+        assert "RELEASE-NAME-prometheus" == jmespath.search("metadata.name", docs[2])
+        assert "ClusterIP" == jmespath.search("spec.type", docs[2])
+        assert {
+            "name": "auth-proxy",
+            "protocol": "TCP",
+            "port": 8084,
+        } in jmespath.search("spec.ports", docs[2])
+
+    def test_authSidecar_kibana(self, kube_version):
+        """Test External ElasticSearch with secret passed from config/values.yaml."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"authSidecar": {"enabled": True}}},
+            show_only=[
+                "charts/kibana/templates/kibana-deployment.yaml",
+                "charts/kibana/templates/kibana-auth-sidecar-configmap.yaml",
+                "charts/kibana/templates/kibana-service.yaml",
+                "charts/kibana/templates/kibana-networkpolicy.yaml",
+                "charts/kibana/templates/ingress.yaml",
+            ],
+        )
+
+        assert len(docs) == 5
+        doc = docs[0]
+        assert doc["kind"] == "Deployment"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-kibana"
+        assert "auth-proxy" == doc["spec"]["template"]["spec"]["containers"][1]["name"]
+
+        assert "Service" == jmespath.search("kind", docs[2])
+        assert "RELEASE-NAME-kibana" == jmespath.search("metadata.name", docs[2])
+        assert "ClusterIP" == jmespath.search("spec.type", docs[2])
+        assert {
+            "name": "auth-proxy",
+            "protocol": "TCP",
+            "port": 8084,
+        } in jmespath.search("spec.ports", docs[2])
