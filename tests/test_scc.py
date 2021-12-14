49ac0a89ffa3827f2c8d03b46fa4f5ca3613c284
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-14 14:52:16 -0800

    Add some tests for scc

---
+++ tests/test_scc.py
@@ -0,0 +1,58 @@
+from tests.helm_template_generator import render_chart
+import pytest
+from . import supported_k8s_versions
+import yaml
+
+
+show_only = [
+    "charts/astronomer/templates/commander/commander-role.yaml",
+    "charts/astronomer/templates/houston/houston-configmap.yaml",
+]
+
+
+commander_expected_result = {
+    "apiGroups": ["security.openshift.io"],
+    "resources": ["securitycontextconstraints"],
+    "verbs": ["create", "delete"],
+}
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+def test_scc_disabled(kube_version):
+    """
+    Test all things scc related when scc is disabled.
+    """
+    docs = render_chart(
+        kube_version=kube_version,
+        values={"global": {"sccEnabled": False}},
+        show_only=show_only,
+    )
+    houston_values = yaml.safe_load(docs[1]["data"]["production.yaml"])
+
+    assert len(docs) == 2
+    assert commander_expected_result not in docs[0]["rules"]
+    assert houston_values["deployments"]["helm"].get("sccEnabled") is None
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+def test_scc_enabled(kube_version):
+    """
+    Test all things scc related when scc is disabled.
+    """
+
+    docs = render_chart(
+        kube_version=kube_version,
+        values={"global": {"sccEnabled": True}},
+        show_only=show_only,
+    )
+    houston_values = yaml.safe_load(docs[1]["data"]["production.yaml"])
+
+    assert len(docs) == 2
+    assert commander_expected_result in docs[0]["rules"]
+    assert houston_values["deployments"]["helm"]["sccEnabled"] is True
