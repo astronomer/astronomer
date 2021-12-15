1cb1d59c7688a614eb4639347b34e9e08be7a90f
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-14 18:29:15 -0800

    Migrate stan metrics and resource tests from helm-unittest to pytest.

--- tests/test_stan_sts.py
+++ tests/test_stan_sts.py
@@ -7,32 +7,56 @@ from . import supported_k8s_versions
     "kube_version",
     supported_k8s_versions,
 )
-def test_stan_statefulset(kube_version):
-    """Test that helm renders a good statefulset template for astronomer stan."""
-    docs = render_chart(
-        kube_version=kube_version,
-        show_only=["charts/stan/templates/statefulset.yaml"],
-    )
+class TestStanStatefulSet:
+    def test_stan_statefulset_defaults(self, kube_version):
+        """Test that stan statefulset is good with defaults."""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=["charts/stan/templates/statefulset.yaml"],
+        )
 
-    assert len(docs) == 1
-    doc = docs[0]
-    containers = doc["spec"]["template"]["spec"]["containers"]
-    c_by_name = {c["name"]: c for c in containers}
-    assert doc["kind"] == "StatefulSet"
-    assert doc["apiVersion"] == "apps/v1"
-    assert doc["metadata"]["name"] == "RELEASE-NAME-stan"
-    assert {c["image"] for c in containers} == {
-        "quay.io/astronomer/ap-nats-streaming:0.22.0-2",
-        "quay.io/astronomer/ap-nats-exporter:0.9.0",
-    }
+        assert len(docs) == 1
+        doc = docs[0]
+        containers = doc["spec"]["template"]["spec"]["containers"]
+        c_by_name = {c["name"]: c for c in containers}
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-stan"
+        assert {c["image"] for c in containers} == {
+            "quay.io/astronomer/ap-nats-streaming:0.22.0-2",
+            "quay.io/astronomer/ap-nats-exporter:0.9.0",
+        }
 
-    assert c_by_name["stan"]["livenessProbe"] == {
-        "httpGet": {"path": "/streaming/serverz", "port": "monitor"},
-        "initialDelaySeconds": 10,
-        "timeoutSeconds": 5,
-    }
-    assert c_by_name["stan"]["readinessProbe"] == {
-        "httpGet": {"path": "/streaming/serverz", "port": "monitor"},
-        "initialDelaySeconds": 10,
-        "timeoutSeconds": 5,
-    }
+        assert c_by_name["stan"]["livenessProbe"] == {
+            "httpGet": {"path": "/streaming/serverz", "port": "monitor"},
+            "initialDelaySeconds": 10,
+            "timeoutSeconds": 5,
+        }
+        assert c_by_name["stan"]["readinessProbe"] == {
+            "httpGet": {"path": "/streaming/serverz", "port": "monitor"},
+            "initialDelaySeconds": 10,
+            "timeoutSeconds": 5,
+        }
+
+    def test_stan_statefulset_with_metrics_and_resources(self, kube_version):
+        """Test that stan statefulset renders good metrics exporter."""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=["charts/stan/templates/statefulset.yaml"],
+            values={
+                "stan": {
+                    "exporter": {
+                        "enabled": True,
+                        "resources": {"requests": {"cpu": "234m"}},
+                    },
+                    "stan": {"resources": {"requests": {"cpu": "123m"}}},
+                },
+            },
+        )
+
+        assert len(docs) == 1
+        containers = docs[0]["spec"]["template"]["spec"]["containers"]
+        assert len(containers) == 2
+        c_by_name = {c["name"]: c for c in containers}
+        assert c_by_name["stan"]["resources"]["requests"]["cpu"] == "123m"
+        assert c_by_name["metrics"]["resources"]["requests"]["cpu"] == "234m"
