dabeb247a6249d80de878397a13f98ff17968707
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-14 17:32:38 -0800

    Add a bunch of registry sts tests to replace helm-unittest and more.

--- tests/test_registry_statefulset.py
+++ tests/test_registry_statefulset.py
@@ -49,6 +49,72 @@ class TestRegistryStatefulset:
         )
 
         assert len(docs) == 1
+        doc = docs[0]
+
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-registry"
+        assert doc["spec"]["template"]["spec"]["volumes"][2]["name"] == "gcs-keyfile"
         assert (
-            docs[0]["spec"]["template"]["spec"]["volumes"][2]["name"] == "gcs-keyfile"
+            docs["spec"]["template"]["spec"]["containers"][0]["volumeMounts"][3]["name"]
+            == "gcs-keyfile"
         )
+
+    def test_registry_sts_with_registry_persistence_enabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=self.show_only,
+            values={"astronomer": {"registry": {"s3": {"enabled": True}}}},
+        )
+
+        assert docs[0]["kind"] == "Deployment"
+        assert {"name": "data", "emptyDir": {}} in docs[0]["spec"]["template"]["spec"][
+            "volumes"
+        ]
+
+    def test_registry_sts_with_registry_s3_enabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=self.show_only,
+            values={"astronomer": {"registry": {"s3": {"enabled": True}}}},
+        )
+
+        assert docs[0]["kind"] == "Deployment"
+        assert {"name": "data", "emptyDir": {}} in docs[0]["spec"]["template"]["spec"][
+            "volumes"
+        ]
+
+    def test_registry_sts_with_registry_gcs_enabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=self.show_only,
+            values={"astronomer": {"registry": {"gcs": {"enabled": True}}}},
+        )
+
+        assert docs[0]["kind"] == "Deployment"
+        assert {"name": "data", "emptyDir": {}} in docs[0]["spec"]["template"]["spec"][
+            "volumes"
+        ]
+
+    def test_registry_sts_with_registry_azure_enabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=self.show_only,
+            values={"astronomer": {"registry": {"azure": {"enabled": True}}}},
+        )
+
+        assert docs[0]["kind"] == "Deployment"
+        assert {"name": "data", "emptyDir": {}} in docs[0]["spec"]["template"]["spec"][
+            "volumes"
+        ]
+
+    def test_registry_sts_with_podlabels(self, kube_version):
+        labels = {"foo-key": "foo-value", "bar-key": "bar-value"}
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=self.show_only,
+            values={"astronomer": {"podLabels": labels}},
+        )
+
+        for k, v in labels.items():
+            assert docs[0]["spec"]["template"]["metadata"]["labels"][k] == v
