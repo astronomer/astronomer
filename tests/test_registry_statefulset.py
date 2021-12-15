869679e03335ea50e451b0ca850af1f3a29d5b9e
Daniel Hoherd <daniel.hoherd@gmail.com>
2021-12-14 16:32:07 -0800

    Port registry sts helm-unittest to pytest

--- tests/test_registry_statefulset.py
+++ tests/test_registry_statefulset.py
@@ -8,13 +8,14 @@ from . import supported_k8s_versions
     supported_k8s_versions,
 )
 class TestRegistryStatefulset:
-    def test_basic_cases(self, kube_version):
+
+    show_only = ["charts/astronomer/templates/registry/registry-statefulset.yaml"]
+
+    def test_registry_sts_basic_cases(self, kube_version):
         """Test some things that should apply to all cases."""
         docs = render_chart(
             kube_version=kube_version,
-            show_only=[
-                "charts/astronomer/templates/registry/registry-statefulset.yaml"
-            ],
+            show_only=self.show_only,
         )
         assert len(docs) == 1
 
@@ -33,3 +34,21 @@ class TestRegistryStatefulset:
             },
         }
         assert expected_env in doc["spec"]["template"]["spec"]["containers"][0]["env"]
+
+    def test_registry_sts_use_keyfile(self, kube_version):
+        """Test some things that should apply to all cases."""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=self.show_only,
+            values={
+                "global": {"baseDomain": "example.com"},
+                "astronomer": {
+                    "registry": {"gcs": {"useKeyfile": True, "enabled": True}}
+                },
+            },
+        )
+
+        assert len(docs) == 1
+        assert (
+            docs[0]["spec"]["template"]["spec"]["volumes"][2]["name"] == "gcs-keyfile"
+        )
