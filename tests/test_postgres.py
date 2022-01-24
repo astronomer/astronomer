8239361214ac4cce5bcda434551b789e22c70d58
pgvishnuram <vishnu@astronomer.io>
2022-01-24 20:23:06 +0530

    added test case for postgres volume permission init container validation

---
+++ tests/test_postgres.py
@@ -0,0 +1,44 @@
+from tests.helm_template_generator import render_chart
+import pytest
+from . import supported_k8s_versions
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestPostgresql:
+    def test_postgresql_statefulset_defaults(self, kube_version):
+        """Test postgresql statefulset is good with defaults."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"postgresqlEnabled": True}},
+            show_only=["charts/postgresql/templates/statefulset.yaml"],
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        print(doc["spec"]["template"]["spec"]["containers"])
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-postgresql"
+        assert "initContainers" in doc["spec"]["template"]["spec"]
+
+    def test_postgresql_statefulset_with_volumePermissions_enabled(self, kube_version):
+        """Test postgresql statefulset when volumePermissions init container is enabled."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "global": {"postgresqlEnabled": True},
+                "postgresql.volumePermissions.enabled": True,
+            },
+            show_only=["charts/postgresql/templates/statefulset.yaml"],
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        print(doc["spec"]["template"]["spec"]["containers"])
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-postgresql"
+        assert "initContainers" in doc["spec"]["template"]["spec"]
