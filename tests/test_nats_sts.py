0fcee7cba75a86b6146b6bb57f3c8a3fbeb31c16
Dylan Intorf <dintorf@gmail.com>
2022-02-08 12:06:20 -0700

    Allow nodeSelector, affinity, and tolerations configuration for Stan and Nats (#1347)

--- tests/test_nats_sts.py
+++ tests/test_nats_sts.py
@@ -7,12 +7,12 @@ from . import supported_k8s_versions
     "kube_version",
     supported_k8s_versions,
 )
-class TestStanStatefulSet:
-    def test_stan_statefulset_defaults(self, kube_version):
-        """Test that stan statefulset is good with defaults."""
+class TestNatsStatefulSet:
+    def test_nats_statefulset_defaults(self, kube_version):
+        """Test that nats statefulset is good with defaults."""
         docs = render_chart(
             kube_version=kube_version,
-            show_only=["charts/stan/templates/statefulset.yaml"],
+            show_only=["charts/nats/templates/statefulset.yaml"],
         )
 
         assert len(docs) == 1
@@ -22,20 +22,20 @@ class TestStanStatefulSet:
         }
         assert doc["kind"] == "StatefulSet"
         assert doc["apiVersion"] == "apps/v1"
-        assert doc["metadata"]["name"] == "RELEASE-NAME-stan"
+        assert doc["metadata"]["name"] == "RELEASE-NAME-nats"
         assert c_by_name["metrics"]["image"].startswith(
             "quay.io/astronomer/ap-nats-exporter:"
         )
-        assert c_by_name["stan"]["image"].startswith(
-            "quay.io/astronomer/ap-nats-streaming:"
+        assert c_by_name["nats"]["image"].startswith(
+            "quay.io/astronomer/ap-nats-server:"
         )
-        assert c_by_name["stan"]["livenessProbe"] == {
-            "httpGet": {"path": "/streaming/serverz", "port": "monitor"},
+        assert c_by_name["nats"]["livenessProbe"] == {
+            "httpGet": {"path": "/", "port": 8222},
             "initialDelaySeconds": 10,
             "timeoutSeconds": 5,
         }
-        assert c_by_name["stan"]["readinessProbe"] == {
-            "httpGet": {"path": "/streaming/serverz", "port": "monitor"},
+        assert c_by_name["nats"]["readinessProbe"] == {
+            "httpGet": {"path": "/", "port": 8222},
             "initialDelaySeconds": 10,
             "timeoutSeconds": 5,
         }
@@ -44,18 +44,18 @@ class TestStanStatefulSet:
         assert doc["spec"]["template"]["spec"]["affinity"] == {}
         assert doc["spec"]["template"]["spec"]["tolerations"] == []
 
-    def test_stan_statefulset_with_metrics_and_resources(self, kube_version):
-        """Test that stan statefulset renders good metrics exporter."""
+    def test_nats_statefulset_with_metrics_and_resources(self, kube_version):
+        """Test that nats statefulset renders good metrics exporter."""
         docs = render_chart(
             kube_version=kube_version,
-            show_only=["charts/stan/templates/statefulset.yaml"],
+            show_only=["charts/nats/templates/statefulset.yaml"],
             values={
-                "stan": {
+                "nats": {
                     "exporter": {
                         "enabled": True,
                         "resources": {"requests": {"cpu": "234m"}},
                     },
-                    "stan": {"resources": {"requests": {"cpu": "123m"}}},
+                    "nats": {"resources": {"requests": {"cpu": "123m"}}},
                 },
             },
         )
@@ -64,13 +64,13 @@ class TestStanStatefulSet:
         containers = docs[0]["spec"]["template"]["spec"]["containers"]
         assert len(containers) == 2
         c_by_name = {c["name"]: c for c in containers}
-        assert c_by_name["stan"]["resources"]["requests"]["cpu"] == "123m"
+        assert c_by_name["nats"]["resources"]["requests"]["cpu"] == "123m"
         assert c_by_name["metrics"]["resources"]["requests"]["cpu"] == "234m"
 
-    def test_stan_statefulset_with_affinity_and_tolerations(self, kube_version):
-        """Test that stan statefulset renders proper nodeSelector, affinity, and tolerations"""
+    def test_nats_statefulset_with_affinity_and_tolerations(self, kube_version):
+        """Test that nats statefulset renders proper nodeSelector, affinity, and tolerations"""
         values = {
-            "stan": {
+            "nats": {
                 "nodeSelector": {"role": "astro"},
                 "affinity": {
                     "nodeAffinity": {
@@ -100,7 +100,7 @@ class TestStanStatefulSet:
         }
         docs = render_chart(
             kube_version=kube_version,
-            show_only=["charts/stan/templates/statefulset.yaml"],
+            show_only=["charts/nats/templates/statefulset.yaml"],
             values=values,
         )
 
@@ -118,10 +118,10 @@ class TestStanStatefulSet:
             == 1
         )
         assert len(spec["tolerations"]) > 0
-        assert spec["tolerations"] == values["stan"]["tolerations"]
+        assert spec["tolerations"] == values["nats"]["tolerations"]
 
-    def test_stan_statefulset_with_global_affinity_and_tolerations(self, kube_version):
-        """Test that stan statefulset renders proper nodeSelector, affinity, and tolerations with global config"""
+    def test_nats_statefulset_with_global_affinity_and_tolerations(self, kube_version):
+        """Test that nats statefulset renders proper nodeSelector, affinity, and tolerations with global config"""
         values = {
             "global": {
                 "platformNodePool": {
@@ -155,7 +155,7 @@ class TestStanStatefulSet:
         }
         docs = render_chart(
             kube_version=kube_version,
-            show_only=["charts/stan/templates/statefulset.yaml"],
+            show_only=["charts/nats/templates/statefulset.yaml"],
             values=values,
         )
 
