ee68763a177750584045ba5e15dfd0a0e2d5468b
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-02-10 17:52:58 -0800

    Add more tests that apply to all charts.

--- tests/test_default_chart.py
+++ tests/test_default_chart.py
@@ -11,28 +11,65 @@ template_ids = [template["name"] for template in default_chart_data]
 
 
 @pytest.mark.parametrize("template", default_chart_data, ids=template_ids)
-def test_default_chart_with_basedomain(template):
-    """Test that each template used with just baseDomain set renders."""
-    docs = render_chart(
-        show_only=[template["name"]],
-    )
-    assert len(docs) == template["length"]
-
+class TestAllCharts:
     pod_managers = ["Deployment", "StatefulSet", "DaemonSet"]
 
-    pod_manger_docs = [doc for doc in docs if doc["kind"] in pod_managers]
-    for doc in pod_manger_docs:
-        c_by_name = {
-            c["name"]: c for c in doc["spec"]["template"]["spec"].get("containers")
-        }
-        if doc["spec"]["template"]["spec"].get("initContainers"):
-            c_by_name.update(
-                {
-                    c["name"]: c
-                    for c in doc["spec"]["template"]["spec"].get("containers")
+    def test_default_chart_with_basedomain(self, template):
+        """Test that each template used with just baseDomain set renders."""
+        docs = render_chart(
+            show_only=[template["name"]],
+        )
+        assert len(docs) == template["length"]
+
+        pod_manger_docs = [doc for doc in docs if doc["kind"] in self.pod_managers]
+        for doc in pod_manger_docs:
+            c_by_name = {
+                c["name"]: c for c in doc["spec"]["template"]["spec"].get("containers")
+            }
+            if doc["spec"]["template"]["spec"].get("initContainers"):
+                c_by_name.update(
+                    {
+                        c["name"]: c
+                        for c in doc["spec"]["template"]["spec"].get("containers")
+                    }
+                )
+            for name, container in c_by_name.items():
+                assert container[
+                    "image"
+                ], f"container {name} does not have an image: {doc}"
+                assert container[
+                    "imagePullPolicy"
+                ], f"container {name} does not have an imagePullPolicy: {doc}"
+
+    def test_all_charts_with_private_registry(self, template):
+        """Test that each chart uses the privateRegistry."""
+        private_repo = "example.com/the-private-registry-repository"
+        docs = render_chart(
+            show_only=[template["name"]],
+            values={
+                "global": {
+                    "privateRegistry": {
+                        "enabled": True,
+                        "repository": private_repo,
+                    }
                 }
-            )
-        for name, container in c_by_name.items():
-            assert container[
-                "imagePullPolicy"
-            ], f"container {name} does not have an imagePullPolicy: {doc}"
+            },
+        )
+
+        pod_manger_docs = [doc for doc in docs if doc["kind"] in self.pod_managers]
+        for doc in pod_manger_docs:
+            c_by_name = {
+                c["name"]: c for c in doc["spec"]["template"]["spec"].get("containers")
+            }
+            if doc["spec"]["template"]["spec"].get("initContainers"):
+                c_by_name.update(
+                    {
+                        c["name"]: c
+                        for c in doc["spec"]["template"]["spec"].get("containers")
+                    }
+                )
+
+            for name, container in c_by_name.items():
+                assert container["image"].startswith(
+                    private_repo
+                ), f"The container '{name}' does not use the privateRegistry repo '{private_repo}': {container}"
