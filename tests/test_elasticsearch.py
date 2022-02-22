85f4a6b2effd9dea37560117d8f889a8cae072d3
pgvishnuram <vishnu@astronomer.io>
2022-02-22 14:39:59 +0530

    add test cases to validate es sysctl init container

---
+++ tests/test_elasticsearch.py
@@ -0,0 +1,96 @@
+from tests.helm_template_generator import render_chart
+import pytest
+from . import supported_k8s_versions
+import jmespath
+import base64
+
+secret = base64.b64encode(b"sample-secret").decode()
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestElasticSearch:
+    def test_elasticsearch_with_sysctl_defaults(self, kube_version):
+        """Test  ElasticSearch with sysctl config/values.yaml."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={},
+            show_only=[
+                "charts/elasticsearch/templates/master/es-master-statefulset.yaml",
+                "charts/elasticsearch/templates/data/es-data-statefulset.yaml",
+                "charts/elasticsearch/templates/client/es-client-deployment.yaml",
+            ],
+        )
+
+        assert len(docs) == 3
+        doc = docs[0]
+        print(doc)
+        assert doc["kind"] == "StatefulSet"
+        assert "sysctl" == jmespath.search(
+            "spec.template.spec.initContainers[0].name", docs[0]
+        )
+        assert any(
+            "262144" in arg
+            for args in jmespath.search(
+                "spec.template.spec.initContainers[*].command", doc
+            )
+            for arg in args
+        )
+
+        doc = docs[1]
+        assert doc["kind"] == "StatefulSet"
+        assert "sysctl" == jmespath.search(
+            "spec.template.spec.initContainers[0].name", docs[1]
+        )
+        assert any(
+            "262144" in arg
+            for args in jmespath.search(
+                "spec.template.spec.initContainers[*].command", doc
+            )
+            for arg in args
+        )
+
+        doc = docs[2]
+        assert doc["kind"] == "Deployment"
+        assert "sysctl" == jmespath.search(
+            "spec.template.spec.initContainers[0].name", docs[2]
+        )
+        assert any(
+            "262144" in arg
+            for args in jmespath.search(
+                "spec.template.spec.initContainers[*].command", doc
+            )
+            for arg in args
+        )
+
+    def test_elasticsearch_with_sysctl_disabled(self, kube_version):
+        """Test  ElasticSearch with sysctl config/values.yaml."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"elasticsearch": {"sysctlInitContainer": {"enabled": False}}},
+            show_only=[
+                "charts/elasticsearch/templates/master/es-master-statefulset.yaml",
+                "charts/elasticsearch/templates/data/es-data-statefulset.yaml",
+                "charts/elasticsearch/templates/client/es-client-deployment.yaml",
+            ],
+        )
+
+        assert len(docs) == 3
+        doc = docs[0]
+        print(doc)
+        assert doc["kind"] == "StatefulSet"
+        assert "sysctl" not in jmespath.search(
+            "spec.template.spec.initContainers[*].name", docs[0]
+        )
+
+        doc = docs[1]
+        assert doc["kind"] == "StatefulSet"
+        assert "sysctl" not in jmespath.search(
+            "spec.template.spec.initContainers[*].name", docs[1]
+        )
+
+        doc = docs[2]
+        assert doc["kind"] == "Deployment"
+        assert jmespath.search("spec.template.spec.initContainers", docs[2]) is None
