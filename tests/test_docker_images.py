de0c0ec8f3015f036eb70d73a2b4882d5661b277
Mishal Shah <mishal.shah@astronomer.ai>
2022-03-31 17:00:52 +0530

    converting to parameterize test

--- tests/test_docker_images.py
+++ tests/test_docker_images.py
@@ -1,10 +1,10 @@
 import docker
 from tests.helm_template_generator import render_chart
 import jmespath
+import pytest
 
 
-def test_docker_images(docker_client):
-
+def list_docker_images():
     extra_globals = {
         "global.baseDomain": "foo.com",
         "blackboxExporterEnabled": True,
@@ -25,15 +25,20 @@ def test_docker_images(docker_client):
         if docker_image_sublist is not None:
             docker_images = docker_image_sublist + docker_images
 
-    # Pulling docker image test
-    for docker_image in docker_images:
-        if docker_image is not None:
-            docker_image = docker_image.replace('"', "").strip()
-            try:
-                # Pulling docker image
-                image = docker_client.images.pull(docker_image)
-                print(docker_image + ": " + image.id)
-            except docker.errors.APIError as exc:
-                assert (
-                    False
-                ), f"'Unable to pull docker image: {docker_image} | Error: {exc}"
+    return docker_images
+
+
+# Pulling docker image test
+@pytest.mark.parametrize(
+    "docker_image",
+    list_docker_images()
+)
+def test_docker_image(docker_client, docker_image):
+    docker_image = docker_image.replace('"', "").strip()
+    try:
+        # Pulling docker image
+        image = docker_client.images.pull(docker_image)
+    except docker.errors.APIError as exc:
+        assert (
+            False
+        ), f"'Unable to pull docker image: {docker_image} | Error: {exc}"
