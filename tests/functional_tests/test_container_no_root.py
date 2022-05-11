703e865709b6eef07caa71ac356cf0ecdda4bbad
Mishal Shah <100714644+mishah334@users.noreply.github.com>
2022-05-11 17:35:09 +0530

    Update docker containers to run as non-root user (#1503)

--- tests/functional_tests/test_container_no_root.py
+++ tests/functional_tests/test_container_no_root.py
@@ -34,5 +34,8 @@ def test_container_non_root(request, container):
         f"kubectl://{container['pod_name']}?container={container['_name']}&namespace={container['namespace']}"
     )
 
-    user = pod_client.check_output("whoami")
-    assert user != "root"
+    user_info = pod_client.user()
+    assert user_info.name != "root"
+    assert user_info.group != "root"
+    assert user_info.gid != 0
+    assert user_info.uid != 0
