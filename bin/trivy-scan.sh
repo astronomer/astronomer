48b6c6289f1dc88382a811711d0274d24e771d52
Mishal Shah <mishal.shah@astronomer.io>
2022-05-17 17:51:20 +0530

    Adding trivyignore

---
+++ bin/trivy-scan.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+[ "$#" == 1 ] || { echo "ERROR: Must give exactly one thing to scan, in the form of an ap-vendor directory." ; exit 1 ; }
+[ -f /etc/os-release ] && cat /etc/os-release
+
+GIT_ROOT="$(git -C "${0%/*}" rev-parse --show-toplevel)"
+scan_target="$1"
+ignore_file="$2"
+
+set +exo pipefail
+
+trivy \
+  --cache-dir /tmp/workspace/trivy-cache \
+  image \
+  --ignorefile "${GIT_ROOT}/.cirleci/${ignore_file}" \
+  --ignore-unfixed -s HIGH,CRITICAL \
+  --exit-code 1 \
+  --no-progress \
+  "${scan_target}" > "${GIT_ROOT}/trivy-output.txt"
+exit_code=$?
+
+cat "${GIT_ROOT}/trivy-output.txt"
+
+# Trivy cannot detect vulnerabilities not installed by package managers (EG: busybox, buildroot, make install):
+# - https://github.com/aquasecurity/trivy/issues/481 2020-04-30
+if grep -q -i 'OS is not detected' trivy-output.txt ; then
+  echo "Skipping trivy scan because of unsupported OS"
+  exit 0
+fi
+
+exit "${exit_code}"
