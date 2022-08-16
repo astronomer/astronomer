26fdca89023e39b4067df27d2075569a220b6230
Mishal Shah <100714644+mishah334@users.noreply.github.com>
2022-08-16 10:55:21 +0530

    Feature-3617 Defining Resource Limit for Pods (#1619)
    * Add tests for cpu, memory requests, limits. Rename var.
    
    * Enabling resource limit for elasticsearch.
    
    * Enabling resource limit for elasticsearch client.
    
    * Enabling resource limit for alertmanager.
    
    * Enabling resource limit for nats.
    
    * Enabling resource limit for stan.
    
    * Enabling resource limit for fluentd.
    
    * Enabling resource limit for houston init containers.
    
    * Enabling resource limit for external-es-proxy-deployment.
    
    * Enabling resource limit for prometheus.
    
    * Enabling all features for test.
    
    * Enabling resource limit for nginx-deployment-default.
    
    * pre-commit fix.
    
    * Adding resources limit for auth-sidecar
    
    * Adding resources limit for postgresql
    
    * Fixing resource limit for elasticsearch-exporter.
    
    * Fixing resource limit for elasticsearch-client.
    
    * Fixing resource limit for elasticsearch-data.
    
    * Fixing resource limit for external-es-proxy.
    
    * Updating ants and stan resources.
    
    * Updating resources for nats and stan.
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>

--- tests/chart_tests/test_default_chart.py
+++ tests/chart_tests/test_default_chart.py
@@ -1,8 +1,10 @@
+import re
+
 import pytest
 
+import tests.chart_tests as chart_tests
 from tests import get_containers_by_name
 from tests.chart_tests.helm_template_generator import render_chart
-import re
 
 annotation_validator = re.compile(
     "^([^/]+/)?(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])$"
@@ -13,8 +15,10 @@ pod_managers = ["Deployment", "StatefulSet", "DaemonSet"]
 class TestAllPodSpecContainers:
     """Test pod spec containers for some defaults."""
 
-    default_docs = render_chart()
-    default_docs_trimmed = [doc for doc in default_docs if doc["kind"] in pod_managers]
+    chart_values = chart_tests.get_all_features()
+
+    default_docs = render_chart(values=chart_values)
+    pod_manager_docs = [doc for doc in default_docs if doc["kind"] in pod_managers]
     annotated = [x for x in default_docs if x["metadata"].get("annotations")]
 
     @pytest.mark.parametrize(
@@ -33,15 +37,21 @@ class TestAllPodSpecContainers:
 
     @pytest.mark.parametrize(
         "doc",
-        default_docs_trimmed,
-        ids=[f"{x['kind']}/{x['metadata']['name']}" for x in default_docs_trimmed],
+        pod_manager_docs,
+        ids=[f"{x['kind']}/{x['metadata']['name']}" for x in pod_manager_docs],
     )
     def test_default_chart_with_basedomain(self, doc):
-        """Test that each container in each pod spec renders."""
+        """Test that each container in each pod spec renders and has some required fields."""
         c_by_name = get_containers_by_name(doc, include_init_containers=True)
         for name, container in c_by_name.items():
             assert container["image"], f"container {name} does not have an image: {doc}"
-            assert container["imagePullPolicy"]
+            assert container["imagePullPolicy"] == "IfNotPresent"
+
+            resources = c_by_name[name]["resources"]
+            assert "cpu" in resources.get("limits")
+            assert "memory" in resources.get("limits")
+            assert "cpu" in resources.get("requests")
+            assert "memory" in resources.get("requests")
 
     private_repo = "example.com/the-private-registry-repository"
     private_repo_docs = render_chart(
