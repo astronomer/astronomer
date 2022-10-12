43be1fed3bb079eeada6ca048e8444e5963381dc
Rishabh Karajgi <rishabh.karajgi@gmail.com>
2022-10-13 00:16:44 +0530

    lint fixes

--- tests/chart_tests/test_astronomer_houston_task_metrics.py
+++ tests/chart_tests/test_astronomer_houston_task_metrics.py
@@ -9,54 +9,44 @@ from tests import supported_k8s_versions
     supported_k8s_versions,
 )
 class TestAstronomerHoustonTaskMetricsCronjobs:
-  def test_astronomer_cleanup_task_usage_cron_defaults(self, kube_version):
-    # First rbacEnabled set to true and namespacePools disabled, should create a ClusterRole and ClusterRoleBinding
-      docs = render_chart(
-          kube_version=kube_version,
-          values={
-              "global": {
-                "taskUsageMetricsEnabled" : False
-              }
-          },
-          show_only=[
-              "charts/astronomer/templates/houston/cronjobs/houston-cleanup-task-data-cronjob.yaml",
-          ],
-      )
-      assert len(docs) == 0
-    
-  def test_astronomer_cleanup_task_usage_cron_feature_enabled(self, kube_version):
-  # First rbacEnabled set to true and namespacePools disabled, should create a ClusterRole and ClusterRoleBinding
-    docs = render_chart(
-        kube_version=kube_version,
-        values={
-            "global": {
-              "taskUsageMetricsEnabled" : True
-            }
-        },
-        show_only=[
-            "charts/astronomer/templates/houston/cronjobs/houston-cleanup-task-data-cronjob.yaml",
-        ],
-    )
+    def test_astronomer_cleanup_task_usage_cron_defaults(self, kube_version):
+        # First rbacEnabled set to true and namespacePools disabled, should create a ClusterRole and ClusterRoleBinding
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"taskUsageMetricsEnabled": False}},
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-cleanup-task-data-cronjob.yaml",
+            ],
+        )
+        assert len(docs) == 0
 
-    assert len(docs) == 1
-    assert docs[0]["kind"] == "CronJob"
-    assert docs[0]["metadata"]["name"] == "release-name-houston-cleanup-task-usage-data"
-    assert docs[0]["spec"]["schedule"] == "40 23 * * *"
-  
-  def test_houston_configmap_with_taskusagemetrics_enabled(self, kube_version):
-    """Validate the houston configmap and its embedded data with
-    taskUsageMetricsEnabled."""
+    def test_astronomer_cleanup_task_usage_cron_feature_enabled(self, kube_version):
+        # First rbacEnabled set to true and namespacePools disabled, should create a ClusterRole and ClusterRoleBinding
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"taskUsageMetricsEnabled": True}},
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-cleanup-task-data-cronjob.yaml",
+            ],
+        )
 
-    docs = render_chart(
-        values={
-            "global": {
-                "taskUsageMetricsEnabled": True
-            }
-        },
-        show_only=["charts/astronomer/templates/houston/houston-configmap.yaml"],
-    )
+        assert len(docs) == 1
+        assert docs[0]["kind"] == "CronJob"
+        assert (
+            docs[0]["metadata"]["name"]
+            == "release-name-houston-cleanup-task-usage-data"
+        )
+        assert docs[0]["spec"]["schedule"] == "40 23 * * *"
 
-    doc = docs[0]
-    prod_yaml = yaml.safe_load(doc["data"]["production.yaml"])
-    assert prod_yaml["deployments"]["taskUsageMetricsEnabled"] is True
-      
+    def test_houston_configmap_with_taskusagemetrics_enabled(self, kube_version):
+        """Validate the houston configmap and its embedded data with
+        taskUsageMetricsEnabled."""
+
+        docs = render_chart(
+            values={"global": {"taskUsageMetricsEnabled": True}},
+            show_only=["charts/astronomer/templates/houston/houston-configmap.yaml"],
+        )
+
+        doc = docs[0]
+        prod_yaml = yaml.safe_load(doc["data"]["production.yaml"])
+        assert prod_yaml["deployments"]["taskUsageMetricsEnabled"] is True
