b5362b93eeceeac4c75b960d7456e5825f3ffebb
pgvishnuram <vishnu@astronomer.io>
2022-10-18 21:26:02 +0530

    fix missing tests

--- tests/chart_tests/test_fluentd_index_configmap.py
+++ tests/chart_tests/test_fluentd_index_configmap.py
@@ -1,5 +1,6 @@
 from tests.chart_tests.helm_template_generator import render_chart
 import pytest
+import yaml
 from tests import supported_k8s_versions
 
 
@@ -7,9 +8,8 @@ from tests import supported_k8s_versions
     "kube_version",
     supported_k8s_versions,
 )
-def test_registry_configmap(kube_version):
-    """Test that helm renders an expected registry-configmap to validate
-    regionendpoint flag."""
+def test_fluentd_index_configmap(kube_version):
+    """Test to validate  fluentd index configmap."""
     docs = render_chart(
         kube_version=kube_version,
         show_only=["charts/fluentd/templates/fluentd-index-template-configmap.yaml"],
@@ -19,3 +19,9 @@ def test_registry_configmap(kube_version):
     doc = docs[0]
     assert doc["kind"] == "ConfigMap"
     assert doc["apiVersion"] == "v1"
+    assert doc["metadata"]["name"] == "release-name-fluentd-index-template-configmap"
+    index_cm = yaml.safe_load(doc["data"]["index_template.json"])
+    assert index_cm == {
+        "index_patterns": ["fluentd.*"],
+        "mappings": {"properties": {"date_nano": {"type": "date_nanos"}}},
+    }
