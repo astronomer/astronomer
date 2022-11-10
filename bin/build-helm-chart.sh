30436e03ef486af304e68f68c434762bdddc8dec
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-11-10 15:43:12 -0500

    Make sed -i calls work with GNU and BSD. Create repo_state.log during helm packaging.

--- bin/build-helm-chart.sh
+++ bin/build-helm-chart.sh
@@ -17,8 +17,9 @@ find "${TEMPDIR}/astronomer/charts" -name requirements.yaml -execdir helm dep up
 if [[ ! "${CIRCLE_BRANCH}" =~ release-[0-9]+\.[0-9]+ ]] ; then
   version=$(awk '$1 ~ /^version/ {printf "%s-build%s\n", $2, ENVIRON["CIRCLE_BUILD_NUM"]}' "${TEMPDIR}/astronomer/Chart.yaml")
   echo "Building helm chart for CIRCLE_BUILD_NUM $CIRCLE_BUILD_NUM version ${version}"
-  sed -Ei "/(^version|appVersion): /s/^(version|appVersion): .*/\1: $version/" "${TEMPDIR}/astronomer/Chart.yaml" "${TEMPDIR}/astronomer/charts/astronomer/Chart.yaml"
-  sed -i "s#^description: .*#description: $(date "+%FT%T%z") ${CIRCLE_BRANCH} ${CIRCLE_BUILD_URL} https://github.com/astronomer/astronomer/commits/${CIRCLE_SHA1}#" "${TEMPDIR}/astronomer/Chart.yaml"
+  sed -Ei='' "/(^version|appVersion): /s/^(version|appVersion): .*/\1: $version/" "${TEMPDIR}/astronomer/Chart.yaml" "${TEMPDIR}/astronomer/charts/astronomer/Chart.yaml"
+  sed -i='' "s#^description: .*#description: $(date "+%FT%T%z") ${CIRCLE_BRANCH} ${CIRCLE_BUILD_URL} https://github.com/astronomer/astronomer/commits/${CIRCLE_SHA1}#" "${TEMPDIR}/astronomer/Chart.yaml"
+  bin/repo-state-report.sh > "${TEMPDIR}/astronomer/repo_state.log"
 fi
 
 helm package "${TEMPDIR}/astronomer"
