acabb18667dbe8b851ba779555f1f4c562264ba0
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-11-24 15:10:35 -0500

    Add CircleCI info (#1744)
    * Add CircleCI info
    
    * Add machine_image_version to circleci generator. Remove pyenv since we're well past python2.

--- bin/repo-state-report.sh
+++ bin/repo-state-report.sh
@@ -5,9 +5,20 @@ GIT_ORIGIN=$(git config --get remote.origin.url)
 GIT_SHA=$(git rev-parse HEAD)$(if [[ -n "$(git status --porcelain)" ]] ; then echo " DIRTY" ; fi)
 BUILD_DATE_ISO8601=$(date +%FT%T%z)
 
-cat <<EOF
-Git repo:   ${GIT_ORIGIN}
-Git branch: ${GIT_BRANCH}
-Git SHA:    ${GIT_SHA}
-Build time: ${BUILD_DATE_ISO8601}
-EOF
+output=(
+    "Build time:  ${BUILD_DATE_ISO8601}"
+    "Git repo:    ${GIT_ORIGIN}"
+    "Git branch:  ${GIT_BRANCH}"
+    "Git SHA:     ${GIT_SHA}"
+)
+
+[[ -n "${CIRCLE_BUILD_URL}" ]] && output+=( "CircleCI Build URL:       ${CIRCLE_BUILD_URL}" )
+[[ -n "${CIRCLE_BUILD_NUM}" ]] && output+=( "CircleCI Build Number:    ${CIRCLE_BUILD_NUM}" )
+[[ -n "${CIRCLE_REPOSITORY_URL}" ]] && output+=( "CircleCI Repository URL:  ${CIRCLE_REPOSITORY_URL}" )
+[[ -n "${CIRCLE_SHA1}" ]] && output+=( "CircleCI SHA1:            ${CIRCLE_SHA1}" )
+[[ -n "${CIRCLE_WORKFLOW_ID}" ]] && output+=( "CircleCI Workflow ID:     ${CIRCLE_WORKFLOW_ID}" )
+[[ -n "${CIRCLE_JOB}" ]] && output+=( "CircleCI Job:             ${CIRCLE_JOB}" )
+
+for item in "${output[@]}" ; do
+    echo "$item"
+done
