02b3ea16c9830eb8e292fe0cf98c921efa324f6f
Mishal Shah <mishal.shah@astronomer.io>
2022-12-02 11:48:16 +0530

    Pushing scan result to report service.

--- bin/trivy-scan.sh
+++ bin/trivy-scan.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-[ "$#" == 2 ] || { echo "ERROR: Must give exactly two arguments: <image_to_scan> <ignore_file>" ; exit 1 ; }
+[ "$#" == 2 ] || {
+  echo "ERROR: Must give exactly two arguments: <image_to_scan> <ignore_file>"
+  exit 1
+}
 [ -f /etc/os-release ] && cat /etc/os-release
 
 GIT_ROOT="$(git -C "${0%/*}" rev-parse --show-toplevel)"
@@ -17,16 +20,29 @@ trivy \
   --ignore-unfixed \
   --exit-code 1 \
   --no-progress \
-  "${scan_target}" > "${GIT_ROOT}/trivy-output.txt"
+  --format json \
+  "${scan_target}" >"${GIT_ROOT}/trivy-output.txt"
 exit_code=$?
 
 cat "${GIT_ROOT}/trivy-output.txt"
 
 # Trivy cannot detect vulnerabilities not installed by package managers (EG: busybox, buildroot, make install):
 # - https://github.com/aquasecurity/trivy/issues/481 2020-04-30
-if grep -q -i 'OS is not detected' trivy-output.txt ; then
-  echo "Skipping trivy scan because of unsupported OS"
-  exit 0
+if grep -q -i 'OS is not detected' trivy-output.txt; then
+  echo "Skipping the Trivy scan because of unsupported OS"
+elif [ "${exit_code}" -gt 0 ]; then
+
+  payload=$(cat "${GIT_ROOT}/trivy-output.txt")
+
+  curl --location --request POST 'https://app.us-east-2.astrosec-dev.astro-7051.com/vulnerabilities/' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{
+      "operation": "create",
+      "scanner": "trivy",
+      "repo": "astronomer/astronomer",
+      "payload": "'"${payload}"'"
+    }'
+
 fi
 
 exit "${exit_code}"
