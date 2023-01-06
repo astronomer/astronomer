09d54552b44cbe830489fe250980acffb655337e
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-01-06 07:57:50 -0500

    Update some dev components (#1780)
    * Update some dev component versions. Automatically determine CI runner tag from current date.
    
    * Fix shellcheck complaint.

--- .circleci/generate_circleci_config.py
+++ .circleci/generate_circleci_config.py
@@ -4,20 +4,29 @@ DRY."""
 import os
 import subprocess
 from pathlib import Path
+import datetime
 
 from jinja2 import Template
 
 # When adding a new version, look up the most
 # recent patch version on Dockerhub
-# https://hub.docker.com/r/kindest/node/tags
 # This should match what is in tests/__init__.py
-KUBE_VERSIONS = ["1.21.14", "1.22.15", "1.23.13", "1.24.7"]
+# https://hub.docker.com/r/kindest/node/tags
+kube_versions = [
+    "1.21.14",
+    "1.22.15",
+    "1.23.13",
+    "1.24.7",
+]
 # https://circleci.com/docs/2.0/building-docker-images/#docker-version
-CI_REMOTE_DOCKER_VERSION = "20.10.14"
+ci_remote_docker_version = "20.10.18"
+# https://circleci.com/developer/machine/image/ubuntu-2204
+machine_image_version = "ubuntu-2204:2022.10.2"
+ci_runner_version = datetime.datetime.now().strftime("%Y-%m")
 
 
 def list_docker_images(path):
-    command = f"cd {path} && helm template . 2>/dev/null | awk '/image: / {{print $2}}' | sed 's/\"//g' | sort -u"
+    command = f"cd {path} && helm template . -f tests/enable_all_features.yaml 2>/dev/null | awk '/image: / {{print $2}}' | sed 's/\"//g' | sort -u"
     docker_images_output = subprocess.check_output(command, shell=True)
     docker_image_list = docker_images_output.decode("utf-8").strip().split("\n")
 
@@ -36,9 +45,11 @@ def main():
     templated_file_content = Path(config_template_path).read_text()
     template = Template(templated_file_content)
     config = template.render(
-        kube_versions=KUBE_VERSIONS,
+        kube_versions=kube_versions,
         docker_images=docker_images,
-        remote_docker_version=CI_REMOTE_DOCKER_VERSION,
+        machine_image_version=machine_image_version,
+        remote_docker_version=ci_remote_docker_version,
+        ci_runner_version=ci_runner_version,
     )
     with open(config_path, "w") as circle_ci_config_file:
         warning_header = (
