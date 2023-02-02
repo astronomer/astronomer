0a9b44e4b54163d13e0201be49e51a5b25e5134a
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-02-02 13:58:34 -0500

    Simplifly test_container_runasnonroot()

--- tests/chart_tests/test_non_root_user.py
+++ tests/chart_tests/test_non_root_user.py
@@ -2,7 +2,6 @@ import pytest
 
 import tests.chart_tests as chart_tests
 from tests import supported_k8s_versions
-import re
 
 ignore_kind_list = ["Job"]
 ignore_list = [
@@ -10,16 +9,6 @@ ignore_list = [
     "elasticsearch-exporter_metrics-exporter",
     "elasticsearch-nginx_nginx",
 ]
-
-
-def init_test_non_root_user(kube_version: str) -> dict:
-    """Return a dict of container specs for the given k8s version"""
-    chart_values = chart_tests.get_all_features()
-    containers = {}
-    kube_version_containers = chart_tests.get_chart_containers(
-        kube_version, chart_values, ignore_kind_list
-    )
-    return {**containers, **kube_version_containers}
 
 
 @pytest.mark.parametrize(
@@ -30,10 +19,12 @@ class TestProbes:
     def test_container_runasnonroot(self, kube_version):
         """Ensure all containers have runAsNonRoot."""
 
-        containers = init_test_non_root_user(kube_version)
+        containers = chart_tests.get_chart_containers(
+            kube_version, chart_tests.get_all_features(), ignore_kind_list
+        )
 
         for container in containers.values():
-            if re.split("(?<=name-)(.*$)", container["key"])[1] in ignore_list:
+            if container["key"].split("release-name-")[-1] in ignore_list:
                 pytest.skip("Info: Resource needs root access" + container["key"])
             else:
                 assert container.get("securityContext").get("runAsNonRoot") is True
