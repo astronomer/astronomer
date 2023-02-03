958c97c06a362761b20346113161489c4beda265
Mishal Shah <100714644+mishah334@users.noreply.github.com>
2023-02-03 10:53:43 +0530

    Adding QA Helm Chart Release Workflow (#1802)
    * Adding qa-release workflow.
    
    * Adding qa-release build.
    
    * Typo fix
    
    * Typo fix
    
    * Fixing condition.
    
    * Fixing condition.
    
    (cherry picked from commit 58296a3ebb9e620a84200ad963a41469215c7cc1)

--- bin/build-helm-chart.sh
+++ bin/build-helm-chart.sh
@@ -6,6 +6,7 @@
 set -x
 set -e
 
+QA_FEATURE_RELEASE=${1:-false}
 TEMPDIR="${TEMPDIR:-/tmp/astro-temp}"
 git_root="$(git rev-parse --show-toplevel)"
 
@@ -20,6 +21,14 @@ if [[ ! "${CIRCLE_BRANCH}" =~ release-[0-9]+\.[0-9]+ ]] ; then
   sed -Ei='' "/(^version|appVersion): /s/^(version|appVersion): .*/\1: $version/" "${TEMPDIR}/astronomer/Chart.yaml" "${TEMPDIR}/astronomer/charts/astronomer/Chart.yaml"
   sed -i='' "s#^description: .*#description: $(date "+%FT%T%z") ${CIRCLE_BRANCH} ${CIRCLE_BUILD_URL} https://github.com/astronomer/astronomer/commits/${CIRCLE_SHA1}#" "${TEMPDIR}/astronomer/Chart.yaml"
   bin/repo-state-report.sh > "${TEMPDIR}/astronomer/repo_state.log"
+elif [[ "${CIRCLE_BRANCH}" =~ release-[0-9]+\.[0-9]+ ]] ; then
+  if [ "true" == "${QA_FEATURE_RELEASE}" ] ; then
+    version=${CIRCLE_BRANCH/release-/""}-$(date -u +%Y%m%dT%H%M)-$(git rev-parse --short HEAD)
+    echo "Building helm chart for CIRCLE_BUILD_NUM $CIRCLE_BUILD_NUM version ${version}"
+    sed -Ei='' "/(^version|appVersion): /s/^(version|appVersion): .*/\1: $version/" "${TEMPDIR}/astronomer/Chart.yaml" "${TEMPDIR}/astronomer/charts/astronomer/Chart.yaml"
+    sed -i='' "s#^description: .*#description: $(date "+%FT%T%z") ${CIRCLE_BRANCH} ${CIRCLE_BUILD_URL} https://github.com/astronomer/astronomer/commits/${CIRCLE_SHA1}#" "${TEMPDIR}/astronomer/Chart.yaml"
+    bin/repo-state-report.sh > "${TEMPDIR}/astronomer/repo_state.log"
+  fi
 fi
 
 helm package "${TEMPDIR}/astronomer"
