9f0c5c1150c0f2d969f687b8c1353292df07145e
Daniel Hoherd <daniel.hoherd@gmail.com>
2023-02-17 18:23:43 -0500

    Update tests for additional rules

--- tests/chart_tests/test_elasticsearch.py
+++ tests/chart_tests/test_elasticsearch.py
@@ -310,6 +310,8 @@ class TestElasticSearch:
         assert all(
             x in nginx_config
             for x in [
+                "location ~* /_bulk$ { rewrite /_bulk(.*) /fluentd.$remote_user.*/_bulk$1 break;",
+                "location ~* /_count$ { rewrite /_count(.*) /fluentd.$remote_user.*/_count$1 break;",
                 "location ~* /_search$ { rewrite /_search(.*) /fluentd.$remote_user.*/_search$1 break;",
                 "location ~ ^/ { deny all; } } }",
             ]
@@ -335,6 +337,8 @@ class TestElasticSearch:
         assert all(
             x in nginx_config
             for x in [
+                "location ~* /_bulk$ { rewrite /_bulk(.*) /vector.$remote_user.*/_bulk$1 break;",
+                "location ~* /_count$ { rewrite /_count(.*) /vector.$remote_user.*/_count$1 break;",
                 "location ~* /_search$ { rewrite /_search(.*) /vector.$remote_user.*/_search$1 break;",
                 "location ~ ^/ { deny all; } } }",
             ]
