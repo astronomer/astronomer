f4490d7fec995aef4123d08717c204ed324456ca
Mishal Shah <mishal.shah@astronomer.io>
2023-03-16 12:35:42 +0530

    Fixing condition.

--- bin/trigger_rc_tests.py
+++ bin/trigger_rc_tests.py
@@ -60,6 +60,7 @@ def main(circleci_token: str, astro_path: str):
     for file_name in file_list:
         x = re.search("astronomer-.*.tgz", file_name)
         if x is not None and astro_version is None and "rc" in file_name:
+            print(f"INFO: Found file {file_name}")
             astro_version = file_name
 
     if astro_version is None:
