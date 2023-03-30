669a699c61ce087d6f697397adea9f6110ac09ba
Mishal Shah <mishal.shah@astronomer.io>
2023-03-30 17:58:39 +0530

    Improving message.

--- bin/trigger_feature_stack_update.py
+++ bin/trigger_feature_stack_update.py
@@ -111,7 +111,7 @@ def main(circleci_token: str, astro_path: str, branch: str):
             break
 
     if "success" != pipeline_state and "running" != pipeline_state:
-        print(f"Failed to run pipeline. Status {pipeline_state}")
+        print(f"Error: Failed to run pipeline. Last Status: {pipeline_state}")
         raise SystemError(1)
     else:
         raise SystemExit(0)
