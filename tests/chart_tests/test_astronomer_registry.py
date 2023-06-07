bb351a229fc0626fc623d192f13f82da103619dd
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2023-06-07 18:03:05 +0530

    add extraenv support for registry backend service (#1910)
    * add extraenv support for registry backend service
    
    * update registry test case for extraenv
    
    * update test description
    
    * remove values
    
    * Update tests/chart_tests/test_astronomer_registry.py
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>
    
    * Update tests/chart_tests/test_astronomer_registry.py
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>
    
    * Update tests/chart_tests/test_astronomer_registry.py
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>
    
    * fix failing tests
    
    ---------
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>

--- tests/chart_tests/test_astronomer_registry.py
+++ tests/chart_tests/test_astronomer_registry.py
@@ -8,20 +8,42 @@ import jmespath
     "kube_version",
     supported_k8s_versions,
 )
-def test_astronomer_registry_statefulset(kube_version):
-    """Test that helm renders a good statefulset template for astronomer
-    registry."""
-    docs = render_chart(
-        kube_version=kube_version,
-        show_only=["charts/astronomer/templates/registry/registry-statefulset.yaml"],
-    )
+class TestRegistryStatefulset:
+    def test_astronomer_registry_statefulset_defaults(self, kube_version):
+        """Test that helm renders a good statefulset template for astronomer
+        registry."""
+        docs = render_chart(
+            kube_version=kube_version,
+            show_only=[
+                "charts/astronomer/templates/registry/registry-statefulset.yaml"
+            ],
+        )
 
-    assert len(docs) == 1
-    doc = docs[0]
-    assert doc["kind"] == "StatefulSet"
-    assert doc["apiVersion"] == "apps/v1"
-    assert doc["metadata"]["name"] == "release-name-registry"
-    assert any(
-        "quay.io/astronomer/ap-registry:" in item
-        for item in jmespath.search("spec.template.spec.containers[*].image", doc)
-    )
+        assert len(docs) == 1
+        doc = docs[0]
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "release-name-registry"
+        assert any(
+            "quay.io/astronomer/ap-registry:" in item
+            for item in jmespath.search("spec.template.spec.containers[*].image", doc)
+        )
+
+    def test_astronomer_registry_statefulset_with_custom_env(self, kube_version):
+        """Test that helm renders statefulset template for astronomer
+        registry with custom env values."""
+        extra_env = {"name": "TEST_ENV_VAR_876", "value": "test"}
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"astronomer": {"registry": {"extraEnv": [extra_env]}}},
+            show_only=[
+                "charts/astronomer/templates/registry/registry-statefulset.yaml"
+            ],
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        assert doc["kind"] == "StatefulSet"
+        assert doc["apiVersion"] == "apps/v1"
+        assert doc["metadata"]["name"] == "release-name-registry"
+        assert extra_env in doc["spec"]["template"]["spec"]["containers"][0]["env"]
