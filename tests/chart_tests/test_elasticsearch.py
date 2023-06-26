7f63004490464651036773878a87733c39e68d9f
pgvishnuram <vishnu@astronomer.io>
2023-06-26 12:36:01 +0530

    add default roles validation test

--- tests/chart_tests/test_elasticsearch.py
+++ tests/chart_tests/test_elasticsearch.py
@@ -355,3 +355,40 @@ class TestElasticSearch:
         pod_data = doc["spec"]["template"]["spec"]
         assert pod_data["securityContext"]["runAsNonRoot"] is True
         assert pod_data["securityContext"]["runAsUser"] == 2000
+
+    def test_elasticsearch_role_defaults(self, kube_version):
+        """Test ElasticSearch master, data and client with default roles"""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={},
+            show_only=[
+                "charts/elasticsearch/templates/master/es-master-statefulset.yaml",
+                "charts/elasticsearch/templates/data/es-data-statefulset.yaml",
+                "charts/elasticsearch/templates/client/es-client-deployment.yaml",
+            ],
+        )
+        assert len(docs) == 3
+        node_master_roles_env = {
+            "name": "node.roles",
+            "value": "master,ml,remote_cluster_client,",
+        }
+        assert (
+            node_master_roles_env
+            in docs[0]["spec"]["template"]["spec"]["containers"][0]["env"]
+        )
+        node_data_roles_env = {
+            "name": "node.roles",
+            "value": "data,data_cold,data_content,data_frozen,data_hot,data_warm,ml,remote_cluster_client,transform,",
+        }
+        assert (
+            node_data_roles_env
+            in docs[1]["spec"]["template"]["spec"]["containers"][0]["env"]
+        )
+        node_client_roles_env = {
+            "name": "node.roles",
+            "value": "ingest,ml,remote_cluster_client,",
+        }
+        assert (
+            node_client_roles_env
+            in docs[2]["spec"]["template"]["spec"]["containers"][0]["env"]
+        )
