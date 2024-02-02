b6be2f681ceb93464c62804c86e716889956371d
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-02-02 14:48:22 -0500

    Print all verbose messages to stderr. Move helm template to its own function.

--- bin/show-docker-images.py
+++ bin/show-docker-images.py
@@ -31,14 +31,14 @@ def default_spec_parser(doc, args):
     item_containers = get_containers_from_spec(doc["spec"]["template"]["spec"])
 
     if args.verbose:
-        print(f"Processing {doc['kind']} {doc['metadata']['name']}")
+        print(f"Processing {doc['kind']} {doc['metadata']['name']}", file=sys.stderr)
 
     if args.private_registry and "quay.io" in yaml.dump(item_containers):
         print(
-            f'{doc["kind"]} {doc["metadata"]["name"].removeprefix("release-name-")} uses quay.io'
+            f'{doc["kind"]} {doc["metadata"]["name"].removeprefix("release-name-")} uses quay.io instead of private registry'
         )
         if args.verbose:
-            print(json.dumps(doc["spec"]["template"]["spec"]))
+            print(json.dumps(doc["spec"]["template"]["spec"]), file=sys.stderr)
 
     return item_containers
 
@@ -47,7 +47,7 @@ def job_template_spec_parser(doc, args):
     """Parse and report on CronJobs, which have a different structure than other pod managers."""
 
     if args.verbose:
-        print(f"Processing {doc['kind']} {doc['metadata']['name']}")
+        print(f"Processing {doc['kind']} {doc['metadata']['name']}", file=sys.stderr)
 
     item_containers = get_containers_from_spec(
         doc["spec"]["jobTemplate"]["spec"]["template"]["spec"]
@@ -55,15 +55,18 @@ def job_template_spec_parser(doc, args):
 
     if args.private_registry and "quay.io" in yaml.dump(item_containers):
         print(
-            f'{doc["kind"]} {doc["metadata"]["name"].removeprefix("release-name-")} uses quay.io'
+            f'{doc["kind"]} {doc["metadata"]["name"].removeprefix("release-name-")} uses quay.io instead of private registry'
         )
         if args.verbose:
-            print(json.dumps(doc["spec"]["jobTemplate"]["spec"]["template"]["spec"]))
+            print(
+                json.dumps(doc["spec"]["jobTemplate"]["spec"]["template"]["spec"]),
+                file=sys.stderr,
+            )
 
     return item_containers
 
 
-def get_images_from_houston_configmap(doc):
+def get_images_from_houston_configmap(doc, args):
     """Return a list of images used in the houston configmap."""
     houston_config = yaml.safe_load(doc["data"]["production.yaml"])
     keepers = ("authSideCar", "loggingSidecar")
@@ -72,7 +75,34 @@ def get_images_from_houston_configmap(doc):
         f'{items["authSideCar"]["repository"]}:{items["authSideCar"]["tag"]}'
     )
     logging_sidecar_image = f'{items["loggingSidecar"]["image"]}'
-    return (auth_sidecar_image, logging_sidecar_image)
+    images = (auth_sidecar_image, logging_sidecar_image)
+    if args.verbose and any("quay.io" in x for x in images):
+        print(
+            "Houston configmap uses quay.io instead of private registry",
+            file=sys.stderr,
+        )
+    return images
+
+
+def helm_template(args):
+    """Run helm template and return the parsed yaml."""
+    GIT_ROOT = next(
+        iter([x for x in Path(__file__).resolve().parents if (x / ".git").is_dir()]),
+        None,
+    )
+
+    command = "helm template . --set forceIncompatibleKubernetes=true -f tests/enable_all_features.yaml"
+    if args.private_registry:
+        command += (
+            " --set global.privateRegistry.repository=example.com/the-private-registry"
+            " --set global.privateRegistry.enabled=True"
+        )
+
+    if args.verbose:
+        print(f"Running command: {command}", flush=True, file=sys.stderr)
+    output = subprocess.check_output(command, shell=True, cwd=GIT_ROOT).decode("utf-8")
+
+    return list(yaml.safe_load_all(output))
 
 
 def main():
@@ -92,23 +122,7 @@ def main():
     )
     args = parser.parse_args()
 
-    GIT_ROOT = next(
-        iter([x for x in Path(__file__).resolve().parents if (x / ".git").is_dir()]),
-        None,
-    )
-
-    command = "helm template . --set forceIncompatibleKubernetes=true -f tests/enable_all_features.yaml"
-
-    if args.private_registry:
-        command += (
-            " --set global.privateRegistry.repository=example.com/the-private-registry"
-            " --set global.privateRegistry.enabled=True"
-        )
-
-    if args.verbose:
-        print(f"Running: {command}", flush=True, file=sys.stderr)
-    output = subprocess.check_output(command, shell=True, cwd=GIT_ROOT).decode("utf-8")
-    docs = list(yaml.safe_load_all(output))
+    docs = helm_template(args)
 
     containers = set()
 
@@ -122,7 +136,7 @@ def main():
                 containers.update(job_template_spec_parser(doc, args))
             case {"metadata": {"name": "release-name-houston-config"}}:
                 if args.with_houston:
-                    containers.update(get_images_from_houston_configmap(doc))
+                    containers.update(get_images_from_houston_configmap(doc, args))
             case _:
                 pass
 
