696ec28ebbec7fb9433a7120983cb655c1189d24
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-02-07 19:19:09 -0500

    Iterate

--- bin/helpers.sh
+++ bin/helpers.sh
@@ -12,7 +12,7 @@ function get_debugging_info {
   echo "Printing description and logs where containers in pod are not 1/1..."
   for pod in $(kubectl -n "${NAMESPACE}" get pods | grep -vE 'NAME|Completed| ([0-9]+)/\1 ' | awk '{print $1}') ; do
     hr
-    bash -xc "kubectl describe -n '${NAMESPACE}' '$pod'"
+    bash -xc "kubectl describe -n '${NAMESPACE}' pod '$pod'"
     bash -xc "kubectl logs -n '${NAMESPACE}' '$pod' --all-containers=true | tail -n 30"
     hr
   done
