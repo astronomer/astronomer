031001da421b2f1f5f6928dad210529b27cc8b5e
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2024-02-09 19:40:43 +0530

    segregate resource strategy Migration Job (#2124)
    * fix pre-commit
    
    * update migration job
    
    * add test cases

---
+++ tests/chart_tests/test_astronomer_houston_hook_job.py
@@ -0,0 +1,26 @@
+import pytest
+from tests import supported_k8s_versions
+from tests.chart_tests.helm_template_generator import render_chart
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestHoustonHookJob:
+    def test_au_strategy_job_defaults(self, kube_version):
+        """Test AU Strategy Job defaults."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={},
+            show_only=[
+                "charts/astronomer/templates/houston/helm-hooks/houston-au-strategy-job.yaml"
+            ],
+        )
+        assert len(docs) == 1
+        assert docs[0]["kind"] == "Job"
+        assert docs[0]["metadata"]["name"] == "release-name-update-resource-strategy"
+
+        assert docs[0]["spec"]["template"]["spec"]["containers"][0]["args"] == [
+            "update-deployments-resource-mode"
+        ]
