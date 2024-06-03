f93287ca2d7aea9e5564a1f48e568d45a6d4eebb
Simpcyclassy <chioma.onyekpere@gmail.com>
2024-06-03 12:29:15 +0100

    update cron with flag

---
+++ tests/chart_tests/test_astronomer_houston_deploy_revisions.py
@@ -0,0 +1,62 @@
+from tests.chart_tests.helm_template_generator import render_chart
+import pytest
+from tests import supported_k8s_versions
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestAstronomerHoustonDeployRevisionsCronjobs:
+    def test_astronomer_cleanup_deploy_revisons_cron_defaults(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"houston": {"cleanupDeployRevisions": {"enabled": False}}},
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-cleanup-deploy-revisions-cronjob.yaml",
+            ],
+        )
+        assert len(docs) == 0
+
+    def test_astronomer_cleanup_deploy_revisons_cron_feature_enabled(
+        self, kube_version
+    ):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"houston": {"cleanupDeployRevisions": {"enabled": True}}},
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-cleanup-deploy-revisions-cronjob.yaml",
+            ],
+        )
+
+        assert len(docs) == 1
+        assert docs[0]["kind"] == "CronJob"
+        assert (
+            docs[0]["metadata"]["name"]
+            == "release-name-houston-cleanup-deploy-revisions"
+        )
+        assert docs[0]["spec"]["schedule"] == "40 23 * * *"
+
+    def test_astronomer_cleanup_deploy_revisons_cron_custom_schedule(
+        self, kube_version
+    ):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "houston": {"cleanupDeployRevisions": {"enabled": True}},
+                "astronomer": {
+                    "houston": {"cleanupDeployRevisions": {"schedule": "0 23 * * *"}}
+                },
+            },
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-cleanup-deploy-revisions-cronjob.yaml",
+            ],
+        )
+
+        assert len(docs) == 1
+        assert docs[0]["kind"] == "CronJob"
+        assert (
+            docs[0]["metadata"]["name"]
+            == "release-name-houston-cleanup-deploy-revisions"
+        )
+        assert docs[0]["spec"]["schedule"] == "0 23 * * *"
