f952a3ab9d721a957749fc9f7f4ef0c78bd5aac3
pgvishnuram <vishnu@astronomer.io>
2024-06-04 23:26:51 +0530

    add missing test cases for airflow updates check

---
+++ tests/chart_tests/test_cronjob_check_airflow_updates.py
@@ -0,0 +1,80 @@
+from tests.chart_tests.helm_template_generator import render_chart
+import pytest
+from tests import get_cronjob_containerspec_by_name, supported_k8s_versions
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+class TestHoustonCronJobAirflowUpdates:
+    def test_cronjob_airflow_updates_enabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "astronomer": {"houston": {"updateAirflowCheck": {"enabled": True}}}
+            },
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-check-airflow-version-updates-cronjob.yaml"
+            ],
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        job_container_by_name = get_cronjob_containerspec_by_name(docs[0])
+
+        assert doc["kind"] == "CronJob"
+        assert doc["spec"]["schedule"] == "57 * * * *"
+        assert job_container_by_name["update-check"]["args"] == [
+            "yarn",
+            "check-airflow-updates",
+            "--url=https://updates.astronomer.io/astronomer-certified",
+        ]
+        assert job_container_by_name["update-check"]["securityContext"] == {
+            "runAsNonRoot": True
+        }
+
+    def test_cronjob_airflow_updates_enabled_with_securityContext_overrides(
+        self, kube_version
+    ):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "astronomer": {
+                    "securityContext": {"allowPriviledgeEscalation": False},
+                    "houston": {"updateAirflowCheck": {"enabled": True}},
+                }
+            },
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-check-airflow-version-updates-cronjob.yaml"
+            ],
+        )
+
+        assert len(docs) == 1
+        doc = docs[0]
+        job_container_by_name = get_cronjob_containerspec_by_name(docs[0])
+
+        assert doc["kind"] == "CronJob"
+        assert doc["spec"]["schedule"] == "57 * * * *"
+        assert job_container_by_name["update-check"]["args"] == [
+            "yarn",
+            "check-airflow-updates",
+            "--url=https://updates.astronomer.io/astronomer-certified",
+        ]
+        assert job_container_by_name["update-check"]["securityContext"] == {
+            "runAsNonRoot": True,
+            "allowPriviledgeEscalation": False,
+        }
+
+    def test_cronjob_airflow_updates_disabled(self, kube_version):
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "astronomer": {"houston": {"updateAirflowCheck": {"enabled": False}}}
+            },
+            show_only=[
+                "charts/astronomer/templates/houston/cronjobs/houston-check-airflow-version-updates-cronjob.yaml"
+            ],
+        )
+
+        assert len(docs) == 0
