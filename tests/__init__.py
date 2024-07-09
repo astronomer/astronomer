dc0eabd1de4b5ac618b467f03cbc6a050b1aaef9
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-07-09 16:39:02 -0400

    6476 deprecate k8s 1.25, 1.26 (#2243)

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,7 @@ git_root_dir = [x for x in Path(__file__).resolve().parents if (x / ".git").is_d
 ]
 
 metadata = yaml.safe_load((Path(git_root_dir) / "metadata.yaml").read_text())
-# replace all patch versions with 0 so we end up with ['1.26.0', '1.27.0']
+# replace all patch versions with 0 so we end up with ['a.b.0', 'x.y.0']
 supported_k8s_versions = [
     ".".join(x.split(".")[:-1] + ["0"]) for x in metadata["test_k8s_versions"]
 ]
