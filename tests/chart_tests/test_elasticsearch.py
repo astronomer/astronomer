41203492a1da6b9b4c6f512aac5040911d532538
pgvishnuram <vishnu@astronomer.io>
2024-08-05 15:56:15 +0530

    add elasticsearch tet cases for persistance policy

--- tests/chart_tests/test_elasticsearch.py
+++ tests/chart_tests/test_elasticsearch.py
@@ -650,3 +650,34 @@ class TestElasticSearch:
         assert len(docs) == 1
         c_by_name = get_containers_by_name(docs[0])
         assert c_by_name["nginx"]["securityContext"] == {"runAsNonRoot": True}
+
+    def test_elasticsearch_persistentVolumeClaimRetentionPolicy(self, kube_version):
+        test_persistentVolumeClaimRetentionPolicy = {
+            "whenDeleted": "Delete",
+            "whenScaled": "Retain",
+        }
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "elasticsearch": {
+                    "common": {
+                        "persistence": {
+                            "persistentVolumeClaimRetentionPolicy": test_persistentVolumeClaimRetentionPolicy,
+                        },
+                    },
+                },
+            },
+            show_only=[
+                "charts/elasticsearch/templates/master/es-master-statefulset.yaml",
+                "charts/elasticsearch/templates/data/es-data-statefulset.yaml",
+            ],
+        )
+
+        assert len(docs) == 2
+
+        for doc in docs:
+            assert "persistentVolumeClaimRetentionPolicy" in doc["spec"]
+            assert (
+                test_persistentVolumeClaimRetentionPolicy
+                == doc["spec"]["persistentVolumeClaimRetentionPolicy"]
+            )
