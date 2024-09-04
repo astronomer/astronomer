dc08f381985a86abaf35eaf73c01a519e3ab6130
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-09-04 18:46:01 -0400

    Refactor to use comprehensions, remove reliance on jmespath.

--- tests/chart_tests/__init__.py
+++ tests/chart_tests/__init__.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-import jmespath
 import yaml
 
 from tests.chart_tests.helm_template_generator import render_chart
@@ -14,26 +13,34 @@ def get_chart_containers(k8s_version, chart_values, ignore_kind_list=None) -> di
     """Return a dict of pod specs in the form of {k8s_version}_{release_name}-{pod_name}_{container_name}, with some additional metadata."""
     if ignore_kind_list is None:
         ignore_kind_list = []
+
     docs = render_chart(
         kube_version=k8s_version,
         values=chart_values,
     )
 
-    specs = jmespath.search(
-        "[?spec.template.spec.containers].{name: metadata.name, kind: kind, containers: spec.template.spec.containers[*]}",
-        docs,
-    )
+    specs = [
+        {
+            "name": doc.get("metadata", {}).get("name"),
+            "kind": doc.get("kind"),
+            "containers": doc.get("spec", {}).get("template", {}).get("spec", {}).get("containers", []),
+        }
+        for doc in docs
+        if "spec" in doc
+        and "template" in doc["spec"]
+        and "spec" in doc["spec"]["template"]
+        and "containers" in doc["spec"]["template"]["spec"]
+    ]
 
-    container_spec = {}
     ignore_kind_list = [ignore_kind.lower() for ignore_kind in ignore_kind_list]
-    for spec in specs:
-        kind = spec["kind"]
-        if kind.lower() not in ignore_kind_list:
-            name = spec["name"]
-            for container in spec["containers"]:
-                key = f"{k8s_version}_{name}_" + container["name"]
-                container["key"] = key
-                container["kind"] = kind
-                container_spec[key] = container
 
-    return container_spec
+    return {
+        f"{k8s_version}_{spec['name']}_{container['name']}": {
+            **container,
+            "key": f"{k8s_version}_{spec['name']}_{container['name']}",
+            "kind": spec["kind"],
+        }
+        for spec in specs
+        if spec["kind"].lower() not in ignore_kind_list
+        for container in spec["containers"]
+    }
