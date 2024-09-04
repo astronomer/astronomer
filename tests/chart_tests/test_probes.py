02493969bacaafe9a217b1f0ec40fd323195dbff
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-09-04 18:41:30 -0400

    Simplify logic. Add docstring and type hint.

--- tests/chart_tests/test_probes.py
+++ tests/chart_tests/test_probes.py
@@ -7,15 +7,19 @@ ignore_kind_list = []
 ignore_list = []
 
 
-def init_test_probes():
+def init_test_probes() -> dict:
+    """Return a dict of all containers in the chart for the given kubernetes version.
+
+    The keys for this dict are like <version>_<release-name>-<pod>_<container>
+    EG: 1.30.0_release-name-houston-worker_houston
+    """
     chart_values = chart_tests.get_all_features()
 
-    containers = {}
-    for k8s_version in supported_k8s_versions:
-        k8s_version_containers = chart_tests.get_chart_containers(k8s_version, chart_values, ignore_kind_list)
-        containers = {**containers, **k8s_version_containers}
-
-    return containers
+    return {
+        k: v
+        for k8s_version in supported_k8s_versions
+        for k, v in chart_tests.get_chart_containers(k8s_version, chart_values, ignore_kind_list).items()
+    }
 
 
 chart_containers = init_test_probes()
