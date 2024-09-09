5ba1cae26279fbf07ab94f4e682ee62e6e6c2db3
Shubham-astro <shubham.singh@astronomer.io>
2024-09-09 08:12:43 +0000

    fixing pre-commit issues

--- tests/chart_tests/test_BYO_SAs.py
+++ tests/chart_tests/test_BYO_SAs.py
@@ -3,29 +3,25 @@ import pytest
 from tests import supported_k8s_versions
 from tests.chart_tests.helm_template_generator import render_chart
 
+
 @pytest.mark.parametrize(
     "kube_version",
     supported_k8s_versions,
 )
-
 class TestServiceAccounts:
     def test_SAs_created(self, kube_version):
-        docs = render_chart(
-                kube_version=kube_version,
-                values={"global": {"rbacEnabled": False}}),
-        service_accounts=[]
+        docs = (render_chart(kube_version=kube_version, values={"global": {"rbacEnabled": False}}),)
+        service_accounts = []
         for doc in docs:
             if isinstance(doc, dict) and doc.get("kind") == "ServiceAccount":
                 service_accounts.append(doc["metadata"]["name"])
         assert len(service_accounts) == 0
-        #print(service_accounts)      
+        # print(service_accounts)
 
-    def test_role_created(self,kube_version):
+    def test_role_created(self, kube_version):
         roles = []
         rolebindings = []
-        docs = render_chart(
-                kube_version=kube_version,
-                values={"global": {"rbacEnabled": False}}),
+        docs = (render_chart(kube_version=kube_version, values={"global": {"rbacEnabled": False}}),)
         for doc in docs:
             if isinstance(doc, dict):
                 if doc.get("kind") == "Role":
@@ -34,11 +30,3 @@ class TestServiceAccounts:
                     rolebindings.append(doc["metadata"]["name"])
         assert len(roles) == 0
         assert len(rolebindings) == 0
-     
-
-
-        
-
-
-
-
