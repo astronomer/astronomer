1b1087da747813fed755a71b3079a0afc26ce17a
Shubham-astro <shubham.singh@astronomer.io>
2024-10-25 14:04:39 +0530

    Fix typo in  configsyncer service account template (#2373)
    * Fixing configsyncer typo
    
    * adding tests
    
    * test name change

--- tests/chart_tests/test_byo_sa.py
+++ tests/chart_tests/test_byo_sa.py
@@ -23,3 +23,29 @@ class TestServiceAccounts:
 
         docs = [doc for doc in render_chart(kube_version=kube_version, values=values) if doc["kind"] in ["RoleBinding", "Role"]]
         assert not docs
+
+    def test_serviceaccount_with_overrides(self, kube_version):
+        "Test that if custom SA are added it gets created"
+        values = {
+            "astronomer": {
+                "commander": {"serviceAccount": {"create": "true", "name": "commander-test"}},
+                "registry": {"serviceAccount": {"create": "true", "name": "registry-test"}},
+                "configSyncer": {"serviceAccount": {"create": "true", "name": "configsyncer-test"}},
+                "houston": {"serviceAccount": {"create": "true", "name": "houston-test"}},
+            },
+        }
+        docs = render_chart(
+            kube_version=kube_version,
+            values=values,
+            show_only=[
+                "charts/astronomer/templates/commander/commander-serviceaccount.yaml",
+                "charts/astronomer/templates/registry/registry-serviceaccount.yaml",
+                "charts/astronomer/templates/config-syncer/config-syncer-serviceaccount.yaml",
+                "charts/astronomer/templates/houston/api/houston-bootstrap-serviceaccount.yaml",
+            ],
+        )
+
+        assert len(docs) == 4
+        expected_names = {"commander-test", "registry-test", "configsyncer-test", "houston-test"}
+        extracted_names = {doc["metadata"]["name"] for doc in docs if "metadata" in doc and "name" in doc["metadata"]}
+        assert expected_names.issubset(extracted_names)
