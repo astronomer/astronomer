3a63ddfb00fc2332bec19d4848f0d2ac598ecd37
Shubham-astro <shubham.singh@astronomer.io>
2024-11-01 11:00:36 +0530

    Fixing the rolebinding service accounts (#2384)
    * Fixing the rolebinding service accounts
    
    * remove unneeded vars
    
    * fix pre-commit
    
    * adding tests
    
    * updating vars
    
    * fix pre-run commit
    
    * test fix
    
    * kube-state changes
    
    * adding more rolebindings
    
    * Update charts/astronomer/templates/houston/api/houston-bootstrap-rolebinding.yaml
    
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>
    
    * update tests
    
    ---------
    
    Co-authored-by: pgvishnuram <vishnu@astronomer.io>
    Co-authored-by: Daniel Hoherd <daniel.hoherd@gmail.com>

--- tests/chart_tests/test_byo_sa.py
+++ tests/chart_tests/test_byo_sa.py
@@ -49,3 +49,49 @@ class TestServiceAccounts:
         expected_names = {"commander-test", "registry-test", "configsyncer-test", "houston-test"}
         extracted_names = {doc["metadata"]["name"] for doc in docs if "metadata" in doc and "name" in doc["metadata"]}
         assert expected_names.issubset(extracted_names)
+
+    def test_serviceaccount_with_overrides_rolebinding(self, kube_version):
+        "Test that if custom SA are added it gets created"
+        values = {
+            "astronomer": {
+                "commander": {"serviceAccount": {"create": "true", "name": "commander-test"}},
+                "configSyncer": {"serviceAccount": {"create": "true", "name": "configsyncer-test"}},
+                "houston": {"serviceAccount": {"create": "true", "name": "houston-test"}},
+            },
+            "kube-state": {
+                "serviceAccount": {"name": "kube-state-test"},
+            },
+            "fluentd": {"serviceAccount": {"name": "fluentd-test"}},
+            "prometheus": {"serviceAccount": {"name": "prometheus-test"}},
+            "nginx": {"serviceAccount": {"name": "nginx-test"}},
+            "grafana": {"serviceAccount": {"name": "grafana-test"}},
+        }
+        docs = render_chart(
+            kube_version=kube_version,
+            values=values,
+            show_only=[
+                "charts/astronomer/templates/commander/commander-rolebinding.yaml",
+                "charts/astronomer/templates/config-syncer/config-syncer-rolebinding.yaml",
+                "charts/astronomer/templates/houston/api/houston-bootstrap-rolebinding.yaml",
+                "charts/kube-state/templates/kube-state-rolebinding.yaml",
+                "charts/fluentd/templates/fluentd-clusterrolebinding.yaml",
+                "charts/prometheus/templates/prometheus-rolebinding.yaml",
+                "charts/nginx/templates/nginx-rolebinding.yaml",
+                "charts/grafana/templates/grafana-bootstrap-rolebinding.yaml",
+            ],
+        )
+
+        assert len(docs) == 8
+
+        expected_names = {
+            "commander-test",
+            "configsyncer-test",
+            "houston-test",
+            "kube-state-test",
+            "fluentd-test",
+            "prometheus-test",
+            "nginx-test",
+            "grafana-test",
+        }
+        extracted_names = {doc["subjects"][0]["name"] for doc in docs if doc.get("subjects")}
+        assert expected_names.issubset(extracted_names)
