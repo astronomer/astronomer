174e85c1c7ccf00b5968450fa93d9760ca54ff7a
Shubham-astro <shubham.singh@astronomer.io>
2024-11-14 12:34:57 +0530

    added tests

--- tests/chart_tests/test_astronomer_registry_configmap.py
+++ tests/chart_tests/test_astronomer_registry_configmap.py
@@ -47,3 +47,19 @@ def test_astronomer_registry_redirect_disabled(kube_version):
     doc = docs[0]
     parsed_config_yml = yaml.safe_load(doc["data"]["config.yml"])
     assert parsed_config_yml["storage"]["redirect"]["disable"]
+
+
+@pytest.mark.parametrize(
+    "kube_version",
+    supported_k8s_versions,
+)
+def test_astronomer_registry_auth_disabled(kube_version):
+    """Test that auth section is disabled when enableInsecureAuth is set to True"""
+    docs = render_chart(
+        kube_version=kube_version,
+        values={"astronomer": {"registry": {"enableInsecureAuth": True}}},
+        show_only=["charts/astronomer/templates/registry/registry-configmap.yaml"],
+    )
+    assert len(docs) == 1
+    data = docs[0]["data"]["config.yml"]
+    assert "auth:" not in data
