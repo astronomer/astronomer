89c0d0647bf3dd6dc766a1c064406285e87b3d15
pgvishnuram <vishnu@astronomer.io>
2024-11-22 07:01:15 +0530

    update missing templates

---
+++ tests/chart_tests/test_pods_annotation.py
@@ -0,0 +1,48 @@
+import jmespath
+import pytest
+import yaml
+
+import tests.chart_tests as chart_tests
+from tests.chart_tests.helm_template_generator import render_chart
+
+
+def init_test_pod_annotation_configs():
+
+    chart_values = chart_tests.get_all_features()
+    chart_values["global"]["podAnnotations"] = {"app.cloud.io": "astronomer"}
+
+
+    print(chart_values)
+
+    kubernetes_objects = {
+        "StatefulSet": "spec.template.metadata.annotations",
+        "Deployment": "spec.template.metadata.annotations",
+        "CronJob": "spec.jobTemplate.spec.template.metadata.annotations",
+        "Job": "spec.template.metadata.annotations",
+        "DaemonSet": "spec.template.metadata.annotations",
+        "Pod": "metadata.annotations",
+    }
+
+    docs = render_chart(values=chart_values)
+
+    pod_docs = []
+    for key, val in kubernetes_objects.items():
+        pod_docs += jmespath.search(
+            "[?kind == `%s`].{name: metadata.name, kind: kind, chart: metadata.labels.chart, annotations: %s}" % (key, val),
+            docs,
+        )
+
+    return {f'{doc["chart"]}_{doc["kind"]}_{doc["name"]}': doc["annotations"] for doc in pod_docs}
+
+
+test_pod_annotations_configs_data = init_test_pod_annotation_configs()
+
+
+@pytest.mark.parametrize(
+    "pod_annotations",
+    test_pod_annotations_configs_data.values(),
+    ids=test_pod_annotations_configs_data.keys(),
+)
+def test_pod_labels_configs(pod_annotations):
+    """Annotations check for definition."""
+    assert 'astronomer' ==  pod_annotations["app.cloud.io"]
