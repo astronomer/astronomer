c0b04192f909c5f561b6caf1be3478f42eb84a74
Daniel Hoherd <daniel.hoherd@gmail.com>
2024-11-25 14:46:13 -0500

    Verify that test_custom_serviceaccount_names() is 100% failing

--- tests/chart_tests/test_byo_sa.py
+++ tests/chart_tests/test_byo_sa.py
@@ -191,6 +191,6 @@ def test_custom_serviceaccount_names(template_name):
     pm_docs = [doc for doc in docs if doc["kind"] in pod_managers]
     service_accounts = [get_service_account_name_from_doc(doc) for doc in pm_docs]
     assert service_accounts
-    # assert not any(
-    #     (sa_name.startswith("release-name-") or sa_name == "default") for sa_name in service_accounts
-    # ), f"Expected all service accounts to start with 'release-name-' but found {service_accounts} in {template_name}"
+    assert not any(
+        (sa_name.startswith("release-name-") or sa_name == "default") for sa_name in service_accounts
+    ), f"Expected all service accounts to start with 'release-name-' but found {service_accounts} in {template_name}"
