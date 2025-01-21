c062730392d9890c05eaa8db42e09bd537f0948c
Shubham-astro <shubham.singh@astronomer.io>
2025-01-21 18:33:30 +0530

    add nonrootuser test to use class

--- tests/chart_tests/test_non_root_user.py
+++ tests/chart_tests/test_non_root_user.py
@@ -8,19 +8,18 @@ ignore_list = [
     "elasticsearch-exporter_metrics-exporter",
     "elasticsearch-nginx_nginx",
 ]
-
-
 @pytest.mark.parametrize(
     "kube_version",
     supported_k8s_versions,
 )
-def test_container_runasnonroot(kube_version):
-    """Ensure all containers have runAsNonRoot."""
+class TestContainerRunasNonRoot:
+    def test_container_runasnonroot(self, kube_version):
+        """Ensure all containers have runAsNonRoot."""
 
-    containers = chart_tests.get_chart_containers(kube_version, chart_tests.get_all_features())
+        containers = chart_tests.get_chart_containers(kube_version, chart_tests.get_all_features())
 
-    for container in containers.values():
-        if container["key"].split("release-name-")[-1] in ignore_list:
-            pytest.skip("Info: Resource needs root access" + container["key"])
-        else:
-            assert container.get("securityContext").get("runAsNonRoot") is True
+        for container in containers.values():
+            if container["key"].split("release-name-")[-1] in ignore_list:
+                pytest.skip("Info: Resource needs root access" + container["key"])
+            else:
+                assert container.get("securityContext").get("runAsNonRoot") is True
