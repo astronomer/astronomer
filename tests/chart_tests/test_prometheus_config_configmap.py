dd9a06d67403017d5055e28624b0ce1be6a28c03
Shubham-astro <shubham.singh@astronomer.io>
2025-01-27 11:36:59 +0530

    handling whitespaces for release namespaces.

--- tests/chart_tests/test_prometheus_config_configmap.py
+++ tests/chart_tests/test_prometheus_config_configmap.py
@@ -12,7 +12,7 @@ prometheus_job = {
 airflow_scrape_relabel_config = {
     "source_labels": ["__meta_kubernetes_service_label_astronomer_io_platform_release"],
     "action": "keep",
-    "regex": "^astronomer$",
+    "regex": "^\\s*astronomer\\s*$",
 }
 
 
