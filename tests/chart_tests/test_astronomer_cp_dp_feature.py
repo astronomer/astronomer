d8dd18eb3d374059ba311328008f53a44a7d293e
Shubham-astro <shubham.singh@astronomer.io>
2025-04-10 16:48:31 +0530

    adding more tests

--- tests/chart_tests/test_astronomer_cp_dp_feature.py
+++ tests/chart_tests/test_astronomer_cp_dp_feature.py
@@ -34,4 +34,34 @@ class TestAstronomerCpDpFeature:
             values={"global": {"controlplane": {"enabled": False},
                               "dataplane": {"enabled": True}}},
         )
+        cp_resources = self.filter_charts_by_component(charts, "controlplane")
+        assert len(cp_resources) == 0
 
+        dp_resources = self.filter_charts_by_component(charts, "dataplane")
+        assert len(dp_resources) > 0
+
+    def test_astronomer_both_cp_dp_enabled(self, kube_version):
+        """Test when both CP and DP features are enabled."""
+        charts = render_chart(
+            kube_version=kube_version,
+            values={"global": {"controlplane": {"enabled": True},
+                              "dataplane": {"enabled": True}}},
+        )
+        cp_resources = self.filter_charts_by_component(charts, "controlplane")
+        assert len(cp_resources) > 0
+
+        dp_resources = self.filter_charts_by_component(charts, "dataplane")
+        assert len(dp_resources) > 0
+
+    def test_astronomer_both_cp_dp_disabled(self, kube_version):
+        """Test when both CP and DP features are disabled."""
+        charts = render_chart(
+            kube_version=kube_version,
+            values={"global": {"controlplane": {"enabled": False},
+                              "dataplane": {"enabled": False}}},
+        )
+        cp_resources = self.filter_charts_by_component(charts, "controlplane")
+        assert len(cp_resources) == 0
+
+        dp_resources = self.filter_charts_by_component(charts, "dataplane")
+        assert len(dp_resources) == 0
