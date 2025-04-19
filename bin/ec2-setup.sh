68fe0694a988109da43e0a384f0cc2a473fd92b5
Shubham-astro <shubham.singh@astronomer.io>
2025-04-19 15:39:02 +0530

    correcting ec2

--- bin/ec2-setup.sh
+++ bin/ec2-setup.sh
@@ -2,15 +2,15 @@
 # ec2-setup.sh - Setup script for EC2 instance
 
 # Update system packages
-yum update -y
+sudo yum update -y
 
-yum install -y git
+sudo yum install -y git
 
 # Install Python 3.11 (if needed beyond the CircleCI image)
-amazon-linux-extras install python3.11 -y
+sudo yum install python3 -y
 
 # Install Docker (often needed for integration tests)
-amazon-linux-extras install docker -y
+sudo amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 usermod -a -G docker ec2-user
@@ -18,19 +18,16 @@ usermod -a -G docker ec2-user
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
-mv kubectl /usr/local/bin/
+sudo mv kubectl /usr/local/bin/
 
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
 # Install mkcert
 # First install dependencies
-yum install -y golang nss-tools
+sudo yum install -y golang nss-tools
 # Then install mkcert
 go install filippo.io/mkcert@latest
-cp ~/go/bin/mkcert /usr/local/bin/
-
-# Install your application dependencies
-pip install -r /path/to/requirements.txt
+sudo cp ~/go/bin/mkcert /usr/local/bin/
 
 echo "EC2 instance setup complete!"
