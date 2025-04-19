0de91fa96dac9265d7bb70368100785383b80607
Shubham-astro <shubham.singh@astronomer.io>
2025-04-19 16:12:17 +0530

    lint fix

--- bin/ec2-setup.sh
+++ bin/ec2-setup.sh
@@ -21,9 +21,9 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 # For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+[ "$(uname -m)" = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
 # For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-arm64
+[ "$(uname -m)" = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-arm64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
