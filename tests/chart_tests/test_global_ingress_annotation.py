6a520908502df2220834f7b2f2cee2d41042b680
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-05-15 14:57:22 -0400

    Add test to validate that all ingresses support global.extraAnnotations with enablePerHostIngress on and off

--- tests/chart_tests/test_global_ingress_annotation.py
+++ tests/chart_tests/test_global_ingress_annotation.py
@@ -16,9 +16,34 @@ class TestGlobabIngressAnnotation:
             values={"global": {"extraAnnotations": {"route.openshift.io/termination": "passthrough"}}},
             show_only=sorted([str(x.relative_to(git_root_dir)) for x in Path(git_root_dir).rglob("*ingress*.yaml")]),
         )
-        assert len(docs) == 6
+        assert len(docs) == 5
         for doc in docs:
             assert doc["kind"] == "Ingress"
             assert doc["apiVersion"] == "networking.k8s.io/v1"
             assert "passthrough" in doc["metadata"]["annotations"]["route.openshift.io/termination"]
             assert len(doc["metadata"]["annotations"]) >= 4
+
+    @pytest.mark.parametrize(
+        "enable_per_host_ingress",
+        [
+            pytest.param(True, id="enable_per_host_ingress"),
+            pytest.param(False, id="disable_per_host_ingress"),
+        ],
+    )
+    def test_global_ingress_per_host_ingress(self, kube_version, enable_per_host_ingress):
+        """Test global ingress annotation for platform ingress."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={
+                "global": {
+                    "extraAnnotations": {"kubernetes.io/ingress.allow-http": "false"},
+                    "enablePerHostIngress": enable_per_host_ingress,
+                },
+            },
+        )
+
+        ingresses = [doc for doc in docs if doc["kind"] == "Ingress"]
+
+        for doc in ingresses:
+            assert doc["apiVersion"] == "networking.k8s.io/v1"
+            assert doc["metadata"]["annotations"]["kubernetes.io/ingress.allow-http"] == "false"
