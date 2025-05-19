7bcc6627fe17849616051aaed36254fe2dc5e2a4
Shubham-astro <shubham.singh@astronomer.io>
2025-05-19 17:09:17 +0530

    cp_dp test changes

--- tests/chart_tests/test_astronomer_cp_dp_feature.py
+++ tests/chart_tests/test_astronomer_cp_dp_feature.py
@@ -14,49 +14,49 @@ class TestAstronomerCpDpFeature:
         return [chart for chart in charts if chart.get("metadata", {}).get("labels", {}).get("plane") == component]
 
     def test_astronomer_cp_only(self, kube_version):
-        """Test that helm renders the correct templates when only the controlplane is enabled."""
+        """Test that helm renders the correct templates when only the control is enabled."""
         charts = render_chart(
             kube_version=kube_version,
-            values={"global": {"controlplane": {"enabled": True}, "dataplane": {"enabled": False}}},
+            values={"global": {"plane": {"mode": "control"}}},
         )
-        cp_resources = self.filter_charts_by_component(charts, "controlplane")
+        cp_resources = self.filter_charts_by_component(charts, "control")
         assert len(cp_resources) > 0
 
-        dp_resources = self.filter_charts_by_component(charts, "dataplane")
+        dp_resources = self.filter_charts_by_component(charts, "data")
         assert len(dp_resources) == 0
 
     def test_astronomer_dp_only(self, kube_version):
-        """Test that helm renders the correct templates when only the dataplane is enabled."""
+        """Test that helm renders the correct templates when only the data is enabled."""
         charts = render_chart(
             kube_version=kube_version,
-            values={"global": {"controlplane": {"enabled": False}, "dataplane": {"enabled": True}}},
+            values={"global": {"plane": {"mode": "data"}}},
         )
-        cp_resources = self.filter_charts_by_component(charts, "controlplane")
+        cp_resources = self.filter_charts_by_component(charts, "control")
         assert len(cp_resources) == 0
 
-        dp_resources = self.filter_charts_by_component(charts, "dataplane")
+        dp_resources = self.filter_charts_by_component(charts, "data")
         assert len(dp_resources) > 0
 
     def test_astronomer_both_cp_dp_enabled(self, kube_version):
         """Test when both CP and DP features are enabled."""
         charts = render_chart(
             kube_version=kube_version,
-            values={"global": {"controlplane": {"enabled": True}, "dataplane": {"enabled": True}}},
+            values={"global": {"plane": {"mode": "unified"}}},
         )
-        cp_resources = self.filter_charts_by_component(charts, "controlplane")
-        assert len(cp_resources) > 0
-
-        dp_resources = self.filter_charts_by_component(charts, "dataplane")
-        assert len(dp_resources) > 0
+        unified_resources = self.filter_charts_by_component(charts, "unified")
+        assert len(unified_resources) > 0
 
     def test_astronomer_both_cp_dp_disabled(self, kube_version):
         """Test when both CP and DP features are disabled."""
         charts = render_chart(
             kube_version=kube_version,
-            values={"global": {"controlplane": {"enabled": False}, "dataplane": {"enabled": False}}},
+            values={"global": {"plane": {"mode": "RandomValue"}}},
         )
-        cp_resources = self.filter_charts_by_component(charts, "controlplane")
+        cp_resources = self.filter_charts_by_component(charts, "control")
         assert len(cp_resources) == 0
 
-        dp_resources = self.filter_charts_by_component(charts, "dataplane")
+        dp_resources = self.filter_charts_by_component(charts, "data")
         assert len(dp_resources) == 0
+
+        unified_resources = self.filter_charts_by_component(charts, "unified")
+        assert len(unified_resources) == 0
