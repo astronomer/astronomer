67ece0d0c05e03a6db3078aad6a01ceae7171557
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-06-03 09:44:41 -0400

    Remove non-functional cursor generated code

--- tests/functional_tests/test_container_no_root.py
+++ tests/functional_tests/test_container_no_root.py
@@ -21,11 +21,6 @@ def test_container_non_root(request, container):
     if container["_name"] in container_ignore_list:
         pytest.skip("Info: Unsupported container: " + container["_name"])
 
-    """This is the host fixture for testinfra. To read more, please see
-    the testinfra documentation:
-    https://testinfra.readthedocs.io/en/latest/examples.html#test-docker-images
-    """
-
     pod_client = testinfra.get_host(
         f"kubectl://{container['pod_name']}?container={container['_name']}&namespace={container['namespace']}"
     )
