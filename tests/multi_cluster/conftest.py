e1d07cd32de45773005d3b7cedc8ae38f1ee9b9e
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-06-07 16:11:56 -0400

    Add k8s_apps_v1_client fixture

--- tests/multi_cluster/conftest.py
+++ tests/multi_cluster/conftest.py
@@ -284,7 +284,7 @@ def helm_install(kubeconfig: str, values: str | list[str] = f"{git_root_dir}/con
 @pytest.fixture(scope="function")
 def k8s_core_v1_client(request, clusters) -> client.CoreV1Api:
     """
-    Provide a Kubernetes client for the resolved target cluster.
+    Provide a Kubernetes core/v1 client for the resolved target cluster.
 
     :param request: Pytest request object for accessing test metadata.
     :param clusters: Dictionary of cluster names and their kubeconfig file paths.
@@ -294,6 +294,21 @@ def k8s_core_v1_client(request, clusters) -> client.CoreV1Api:
     kubeconfig_file = clusters[cluster_name]
     config.load_kube_config(config_file=kubeconfig_file)
     return client.CoreV1Api()
+
+
+@pytest.fixture(scope="function")
+def k8s_apps_v1_client(request, clusters) -> client.AppsV1Api:
+    """
+    Provide a Kubernetes apps/v1 client for the resolved target cluster.
+
+    :param request: Pytest request object for accessing test metadata.
+    :param clusters: Dictionary of cluster names and their kubeconfig file paths.
+    :return: A Kubernetes AppsV1Api client for the target cluster.
+    """
+    cluster_name = request.param
+    kubeconfig_file = clusters[cluster_name]
+    config.load_kube_config(config_file=kubeconfig_file)
+    return client.AppsV1Api()
 
 
 def create_namespace(kubeconfig_file, namespace="astronomer") -> None:
