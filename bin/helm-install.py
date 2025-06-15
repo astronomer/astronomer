10d2d3c7e7095237a8792f936bc95c8693aa7c93
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-06-15 15:23:04 -0400

    Add script: bin/helm-install.py

---
+++ bin/helm-install.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""Install the current chart into the given cluster."""
+
+import os
+import shlex
+import subprocess
+import sys
+from pathlib import Path
+
+PREREQUISITES = """You MUST set your environment variable TEST_SCENARIO to one of the following values:
+- unified: Install with the unified application mode.
+- data: Install the with the dataplane application mode.
+- control: Install with the controlplane application mode.
+"""
+
+GIT_ROOT_DIR = next(iter([x for x in Path(__file__).resolve().parents if (x / ".git").is_dir()]), None)
+HELM_EXE = Path.home() / ".local" / "share" / "astronomer-software" / "bin" / "helm"
+HELM_INSTALL_TIMEOUT = os.getenv("HELM_INSTALL_TIMEOUT", "10m0s")
+if not all([(TEST_SCENARIO := os.getenv("TEST_SCENARIO")), TEST_SCENARIO in ["unified", "data", "control"]]):
+    print("ERROR: TEST_SCENARIO environment variable is not set!", file=sys.stderr)
+    print(PREREQUISITES, file=sys.stderr)
+    raise SystemExit(1)
+KUBECONFIG_DIR = Path.home() / ".local" / "share" / "astronomer-software" / "kubeconfig"
+KUBECONFIG_DIR.mkdir(parents=True, exist_ok=True)
+KUBECONFIG_FILE = str(KUBECONFIG_DIR / TEST_SCENARIO)
+DEBUG = os.getenv("DEBUG", "").lower() in ["yes", "true", "1"]
+
+
+def run_command(command: str | list) -> str:
+    """
+    Run a shell command and capture its output.
+
+    Args:
+        command: The shell command to execute.
+
+    Returns:
+        The standard output from the command.
+
+    Raises:
+        RuntimeError: If the command fails.
+    """
+    if isinstance(command, list):
+        command = shlex.join(str(x) for x in command)
+    else:
+        command = str(command)
+    result = subprocess.run(command, shell=True, text=True, capture_output=True)
+    if result.returncode != 0:
+        raise RuntimeError(f"Command failed: {command}\n{result.stderr}")
+    return result.stdout.strip()
+
+
+def helm_install(values: str | list[str] = f"{GIT_ROOT_DIR}/configs/local-dev.yaml") -> None:
+    """
+    Install a Helm chart using the provided kubeconfig and values file.
+
+    :param values: Path to the Helm values file or a list of values files.
+    """
+    helm_install_command = [
+        HELM_EXE,
+        "install",
+        "astronomer",
+        "--create-namespace",
+        "--namespace=astronomer",
+        str(GIT_ROOT_DIR),
+        f"--kubeconfig={KUBECONFIG_FILE}",
+        f"--timeout={HELM_INSTALL_TIMEOUT}",
+    ]
+
+    if isinstance(values, str):
+        values = [values]
+
+    for value in values:
+        if isinstance(value, str) and Path(value).exists():
+            helm_install_command.append(f"--values={value}")
+        else:
+            raise ValueError(f"Invalid values file: {value}")
+
+    if DEBUG:
+        helm_install_command.append("--debug")
+
+    run_command(helm_install_command)
+
+
+if __name__ == "__main__":
+    values = [
+        f"{GIT_ROOT_DIR}/configs/local-dev.yaml",
+        f"{GIT_ROOT_DIR}/tests/data_files/scenario-{TEST_SCENARIO}.yaml",
+    ]
+    helm_install(values=values)
