d9b81b4a4730d1a2deff6a02c7358b2c2441f28c
Shubham-astro <shubham.singh@astronomer.io>
2025-07-30 12:20:28 +0530

    test_astronomer_commander_grpc_ingress_default fix

--- tests/chart_tests/test_astronomer_commander_ingress.py
+++ tests/chart_tests/test_astronomer_commander_ingress.py
@@ -17,8 +17,10 @@ class TestAstronomerCommanderIngress:
             show_only=["charts/astronomer/templates/commander/commander-grpc-ingress.yaml"],
         )
         assert len(docs) == 1
+        ingress_docs = [doc for doc in docs if doc["kind"] == "Ingress"]
+
         doc = docs[0]
-        assert doc["kind"] == "IngressClass"
+        assert doc["kind"] == "Ingress"
         assert doc["metadata"]["name"] == "release-name-commander-api-ingress"
         assert doc["metadata"]["labels"]["component"] == "api-ingress"
         assert doc["metadata"]["labels"]["plane"] == "data"
