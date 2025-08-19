3834a8840abac4060615ccbd012c1e07ebb4c516
Shubham-astro <shubham.singh@astronomer.io>
2025-08-19 18:50:28 +0530

    vector test fix

--- tests/chart_tests/test_vector.py
+++ tests/chart_tests/test_vector.py
@@ -92,7 +92,6 @@ class TestVector:
         )
         assert len(docs) == 0
 
-    @pytest.mark.skip("TODO: add configmap to vector and then fix these tests")
     def test_vector_configmap_manual_namespaces_enabled(self, kube_version):
         """Test that when namespace Pools is disabled, and manualNamespaces is enabled, helm renders a vector configmap targeting
         all namespaces."""
@@ -114,13 +113,12 @@ class TestVector:
             show_only=["charts/vector/templates/vector-configmap.yaml"],
         )[0]
 
-        expected_rule = "key $.kubernetes.namespace_name\n    # vector should gather logs from all namespaces if manualNamespaceNamesEnabled is enabled"
-        assert expected_rule in doc["data"]["output.conf"]
+        expected_rule = "match(.kubernetes.pod_namespace, r'^[a-z0-9]([-a-z0-9]*[a-z0-9])?$')"
+        assert expected_rule in doc["data"]["vector-config.yaml"]
 
-    @pytest.mark.skip("TODO: add configmap to vector and then fix these tests")
     def test_vector_configmap_manual_namespaces_and_namespacepools_disabled(self, kube_version):
         """Test that when namespace Pools and manualNamespaceNamesEnabled are disabled, helm renders a default vector configmap
-        looking at an environment variable."""
+        looking at platform label."""
         values = {
             "global": {
                 "manualNamespaceNamesEnabled": False,
@@ -137,38 +135,8 @@ class TestVector:
             values=values,
             show_only=["charts/vector/templates/vector-configmap.yaml"],
         )[0]
-
-        expected_rule = 'key $.kubernetes.namespace_labels.platform\n    pattern "release-name"'
-        assert expected_rule in doc["data"]["output.conf"]
-
-    @pytest.mark.skip("TODO: revisit this test to see if we need this kind of thing with vector. Probably not.")
-    def test_vector_configmap_configure_extra_log_stores(self, kube_version):
-        """Test that when namespace Pools and manualNamespaceNamesEnabled are disabled, helm renders a default vector configmap
-        looking at an environment variable."""
-        values = {
-            "global": {"logging": {"collector": "vector"}},
-            "vector": {
-                "extraLogStores": """
-<store>
-  @type newrelic
-  @log_level info
-  base_uri https://log-api.newrelic.com/log/v1
-  license_key <LICENSE_KEY>
-  <buffer>
-    @type memory
-    flush_interval 5s
-  </buffer>
-</store>
-                    """
-            },
-        }
-        doc = render_chart(
-            kube_version=kube_version,
-            values=values,
-            show_only=["charts/vector/templates/vector-configmap.yaml"],
-        )[0]
-        expected_store = "  <store>\n    @type newrelic\n    @log_level info\n    base_uri https://log-api.newrelic.com/log/v1\n    license_key <LICENSE_KEY>"
-        assert expected_store in doc["data"]["output.conf"]
+        expected_rule = '.kubernetes.namespace_labels.platform == "release-name"'
+        assert expected_rule in doc["data"]["vector-config.yaml"]
 
     def test_vector_securityContext_override(self, kube_version):
         """Test that helm renders a custom securityContext when securityContext is overridden."""
@@ -209,35 +177,25 @@ class TestVector:
         pod_spec = docs[0]["spec"]["template"]["spec"]
         assert pod_spec["securityContext"] == {}
         assert len(pod_spec["containers"]) == 1
-        assert pod_spec["containers"][0]["securityContext"] == {"readOnlyRootFilesystem": True, "runAsUser": 0}
+        assert pod_spec["containers"][0]["securityContext"] == {"runAsUser": 0}
 
-    @pytest.mark.skip("TODO: revisit this test to see if we need this kind of thing with vector.")
     def test_vector_index_defaults(self, kube_version):
         """Test to validate vector index name prefix defaults in vector configmap."""
         docs = render_chart(
             kube_version=kube_version,
-            values={"global": {"rbacEnabled": True}},
+            values={"global": {"logging": {"collector": "vector"}, "rbacEnabled": True}},
             show_only=[
                 "charts/vector/templates/vector-configmap.yaml",
-                "charts/vector/templates/vector-index-template-configmap.yaml",
             ],
         )
 
-        assert len(docs) == 2
+        assert len(docs) == 1
         doc = docs[0]
         assert doc["kind"] == "ConfigMap"
         assert doc["apiVersion"] == "v1"
         assert doc["metadata"]["name"] == "release-name-vector"
-        assert 'vector.${record["release"]}.${Time.at(time).getutc.strftime(@logstash_dateformat)}' in doc["data"]["output.conf"]
-        doc = docs[1]
-        assert doc["kind"] == "ConfigMap"
-        assert doc["apiVersion"] == "v1"
-        assert doc["metadata"]["name"] == "release-name-vector-index-template-configmap"
-        index_cm = yaml.safe_load(doc["data"]["index_template.json"])
-        assert index_cm == {
-            "index_patterns": ["vector.*"],
-            "mappings": {"properties": {"date_nano": {"type": "date_nanos"}}},
-        }
+        expected_index = 'index: "vector.release-name.%Y.%m.%d"'
+        assert expected_index in doc["data"]["vector-config.yaml"]
 
     @pytest.mark.skip("TODO: revisit this test to see if we need this kind of thing with vector.")
     def test_vector_index_overrides(self, kube_version):
@@ -258,7 +216,7 @@ class TestVector:
         assert doc["apiVersion"] == "v1"
         assert doc["metadata"]["name"] == "release-name-vector"
         assert (
-            'astronomer.${record["release"]}.${Time.at(time).getutc.strftime(@logstash_dateformat)}' in doc["data"]["output.conf"]
+            'astronomer.${record["release"]}.${Time.at(time).getutc.strftime(@logstash_dateformat)}' in doc["data"]["vector-config.yaml"]
         )
 
         doc = docs[1]
@@ -340,10 +298,6 @@ class TestVector:
         doc = docs[0]
         c_by_name = get_containers_by_name(doc)
         assert c_by_name["vector"]["name"] == "vector"
-        expected_probe = {
-            'httpGet': {'path': '/health', 'port': 8686}, 
-            'initialDelaySeconds': 30, 
-            'periodSeconds': 10
-        }
+        expected_probe = {"httpGet": {"path": "/health", "port": 8686}, "initialDelaySeconds": 30, "periodSeconds": 10}
         assert c_by_name["vector"]["livenessProbe"] == expected_probe
         assert c_by_name["vector"]["readinessProbe"] == expected_probe
