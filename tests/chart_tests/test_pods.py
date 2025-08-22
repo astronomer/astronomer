d688ea48cf4bd71a34f55b3f8a0c9f1a44442a41
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-08-22 11:45:09 +0700

    Move tests to class. Remove jmespath.

--- tests/chart_tests/test_pods.py
+++ tests/chart_tests/test_pods.py
@@ -1,47 +1,40 @@
-import jmespath
 import pytest
 
-from tests.utils import get_all_features
+from tests.utils import get_all_features, get_pod_template
 from tests.utils.chart import render_chart
 
 
-def init_test_pod_labels_configs():
-    chart_values = get_all_features()
+class TestPodLabelsDefault:
+    @staticmethod
+    def init_test_pod_labels_configs():
+        chart_values = get_all_features()
 
-    kubernetes_objects = {
-        "StatefulSet": "spec.template.metadata.labels",
-        "Deployment": "spec.template.metadata.labels",
-        "CronJob": "spec.jobTemplate.spec.template.metadata.labels",
-        "Job": "spec.template.metadata.labels",
-        "DaemonSet": "spec.template.metadata.labels",
-        "Pod": "metadata.labels",
-    }
+        docs = render_chart(values=chart_values)
 
-    docs = render_chart(values=chart_values)
+        pod_docs = [
+            {
+                "chart": doc.get("metadata", {}).get("labels", {}).get("chart"),
+                "kind": doc.get("kind"),
+                "labels": doc.get("metadata", {}).get("labels", {}),
+                "name": doc.get("metadata", {}).get("name"),
+            }
+            for doc in [y for y in [get_pod_template(x) for x in docs] if y]
+        ]
 
-    pod_docs = []
-    for key, val in kubernetes_objects.items():
-        pod_docs += jmespath.search(
-            "[?kind == `%s`].{name: metadata.name, kind: kind, chart: metadata.labels.chart, labels: %s}" % (key, val),
-            docs,
-        )
+        return {f"{doc['chart']}_{doc['kind']}_{doc['name']}": doc["labels"] for doc in pod_docs}
 
-    return {f"{doc['chart']}_{doc['kind']}_{doc['name']}": doc["labels"] for doc in pod_docs}
+    test_pod_labels_configs_data = init_test_pod_labels_configs()
 
-
-test_pod_labels_configs_data = init_test_pod_labels_configs()
-
-
-@pytest.mark.parametrize(
-    "pod_labels",
-    test_pod_labels_configs_data.values(),
-    ids=test_pod_labels_configs_data.keys(),
-)
-def test_pod_labels_configs(pod_labels):
-    """Labels check for definition."""
-    assert pod_labels is not None
-    assert "app" in pod_labels
-    assert "version" in pod_labels
-    assert "tier" in pod_labels
-    assert "component" in pod_labels
-    assert "release" in pod_labels
+    @pytest.mark.parametrize(
+        "pod_labels",
+        test_pod_labels_configs_data.values(),
+        ids=test_pod_labels_configs_data.keys(),
+    )
+    def test_pod_labels_configs(self, pod_labels):
+        """Labels check for definition."""
+        assert pod_labels is not None
+        assert "app" in pod_labels
+        assert "version" in pod_labels
+        assert "tier" in pod_labels
+        assert "component" in pod_labels
+        assert "release" in pod_labels
