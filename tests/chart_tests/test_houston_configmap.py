335ce5ffafefbde5170c71895a6efdd0ed7ba0fa
Shubham-astro <shubham.singh@astronomer.io>
2025-08-27 10:35:22 +0530

    test fix

--- tests/chart_tests/test_houston_configmap.py
+++ tests/chart_tests/test_houston_configmap.py
@@ -249,7 +249,7 @@ def test_houston_configmap_with_vector_index_prefix_defaults():
     common_test_cases(docs)
     doc = docs[0]
     prod_yaml = yaml.safe_load(doc["data"]["production.yaml"])
-    assert "fluentd" in prod_yaml["deployments"].get("vectorIndexPrefix")
+    assert "vector" in prod_yaml["deployments"].get("vectorIndexPrefix")
 
 
 def test_houston_configmap_with_vector_index_prefix_overrides():
