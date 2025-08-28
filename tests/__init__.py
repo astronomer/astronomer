d0c1b75da37f59ed60b6a222c4214881a63e4bdf
Daniel Hoherd <daniel.hoherd@gmail.com>
2025-08-28 13:30:06 +0700

    7410 houston read only root filesystem (#2807)

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,6 +8,8 @@ git_root_dir = [x for x in Path(__file__).resolve().parents if (x / ".git").is_d
 chart_metadata = yaml.safe_load((Path(git_root_dir) / "metadata.yaml").read_text())
 # replace all patch versions with 0 so we end up with ['a.b.0', 'x.y.0']
 supported_k8s_versions = [".".join([*x.split(".")[:-1], "0"]) for x in chart_metadata["test_k8s_versions"]]
+newest_supported_kube_version = supported_k8s_versions[-1]
+oldest_supported_kube_version = supported_k8s_versions[0]
 k8s_version_too_old = f"1.{int(supported_k8s_versions[0].split('.')[1]) - 1!s}.0"
 k8s_version_too_new = f"1.{int(supported_k8s_versions[-1].split('.')[1]) + 1!s}.0"
 # kubectl is one version old https://kubernetes.io/releases/version-skew-policy/#kubectl
--- tests/chart_tests/test_tags.py
+++ tests/chart_tests/test_tags.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import pytest
 
+from tests import newest_supported_kube_version
 from tests.utils import get_all_features
 from tests.utils.chart import render_chart
 
@@ -31,8 +32,8 @@ chart_values = get_all_features()
 
 
 @pytest.mark.parametrize("template", show_only)
-def test_tags_monitoring_enabled(template, chart_values=chart_values, kube_version="1.30.0"):
-    """Test that when monitoring is disabled, the monitoring components are not present."""
+def test_tags_monitoring_enabled(template, chart_values=chart_values, kube_version=newest_supported_kube_version):
+    """Test that when monitoring is enabled, the monitoring components are not present."""
     chart_values["tags"] = {"monitoring": True}
     docs = render_chart(kube_version=kube_version, values=chart_values, show_only=template)
 
@@ -40,8 +41,8 @@ def test_tags_monitoring_enabled(template, chart_values=chart_values, kube_versi
 
 
 @pytest.mark.parametrize("template", show_only)
-def test_tags_monitoring_disabled(template, chart_values=chart_values, kube_version="1.30.0"):
-    """Test that when monitoring is disabled, the monitoring components are not present."""
+def test_tags_monitoring_disabled(template, chart_values=chart_values, kube_version=newest_supported_kube_version):
+    """Test that when monitoring is disabled, no monitoring components are present."""
     chart_values["tags"] = {"monitoring": False}
 
     with pytest.raises(subprocess.CalledProcessError):
