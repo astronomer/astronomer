e4d37b4702914de77719c78532ce1ffb574d4e54
pgvishnuram <vishnu@astronomer.io>
2025-08-29 13:53:05 +0530

    fix tests

--- tests/chart_tests/test_vector.py
+++ tests/chart_tests/test_vector.py
@@ -258,7 +258,7 @@ class TestVector:
         doc = docs[0]
         assert doc["kind"] == "ConfigMap"
         assert doc["apiVersion"] == "v1"
-        assert doc["metadata"]["name"] == "release-name-vector"
+        assert doc["metadata"]["name"] == "release-name-vector-config"
         expected_index = 'index: "fluentd.{{ .release }}.%Y.%m.%d"'
         assert expected_index in doc["data"]["vector-config.yaml"]
 
@@ -279,7 +279,7 @@ class TestVector:
         doc = docs[0]
         assert doc["kind"] == "ConfigMap"
         assert doc["apiVersion"] == "v1"
-        assert doc["metadata"]["name"] == "release-name-vector"
+        assert doc["metadata"]["name"] == "release-name-vector-config"
         assert (
             'astronomer.${record["release"]}.${Time.at(time).getutc.strftime(@logstash_dateformat)}'
             in doc["data"]["vector-config.yaml"]
