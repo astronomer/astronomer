2e84c7cb3ce200a6b4e88728adcb2965745e62b4
Shubham-astro <shubham.singh@astronomer.io>
2025-09-02 17:35:52 +0530

    add env tests

--- tests/chart_tests/test_astronomer_commander.py
+++ tests/chart_tests/test_astronomer_commander.py
@@ -54,6 +54,9 @@ class TestAstronomerCommander:
         assert env_vars["COMMANDER_UPGRADE_TIMEOUT"] == "600"
         assert "COMMANDER_MANAGE_NAMESPACE_RESOURCE" not in env_vars
 
+        assert env_vars["COMMANDER_ELASTICSEARCH_ENABLED"] == "true"
+        assert env_vars["COMMANDER_ELASTICSEARCH_LOG_LEVEL"] == "info"
+        assert env_vars["COMMANDER_ELASTICSEARCH_NODE"] == "https://elasticsearch.custom-dp-123.example.com"
         assert env_vars["COMMANDER_HEALTH_STATUS"] == "HEALTHY"
         assert env_vars["COMMANDER_STATUS"] == "HEALTHY"
         assert env_vars["COMMANDER_AIRFLOW_CHART_VERSION"] == "99.88.77"
