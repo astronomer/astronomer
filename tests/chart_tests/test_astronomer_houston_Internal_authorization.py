9ce0563aa97ba54092a164814f5b112482050b72
pgvishnuram <vishnu@astronomer.io>
2025-09-13 05:24:58 +0530

    update modes tests

--- tests/chart_tests/test_astronomer_houston_Internal_authorization.py
+++ tests/chart_tests/test_astronomer_houston_Internal_authorization.py
@@ -29,6 +29,26 @@ class TestHoustonInternalAuthorization:
                 in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
             )
 
+    def test_ingress_with_authorization_with_control_mode(self, kube_version):
+        """Test Alertmanager Service with authSidecar."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"plane": {"mode": "control"}}},
+            show_only=[
+                "charts/alertmanager/templates/ingress.yaml",
+                "charts/prometheus/templates/ingress.yaml",
+            ],
+        )
+
+        assert len(docs) == 2
+        for doc in docs:
+            assert doc["kind"] == "Ingress"
+            assert doc["apiVersion"] == "networking.k8s.io/v1"
+            assert (
+                "http://release-name-houston.default.svc.cluster.local:8871/v1/authorization"
+                in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
+            )
+
     def test_ingress_with_authorization_with_dataplane_mode(self, kube_version):
         """Test Alertmanager Service with authSidecar."""
         docs = render_chart(
