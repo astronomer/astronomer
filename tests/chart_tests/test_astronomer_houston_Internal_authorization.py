74b0cd7448fc30f5337386494f44f65518b6a048
pgvishnuram <vishnu@astronomer.io>
2025-09-13 05:15:54 +0530

    fix tests

--- tests/chart_tests/test_astronomer_houston_Internal_authorization.py
+++ tests/chart_tests/test_astronomer_houston_Internal_authorization.py
@@ -25,26 +25,6 @@ class TestHoustonInternalAuthorization:
             assert doc["kind"] == "Ingress"
             assert doc["apiVersion"] == "networking.k8s.io/v1"
             assert (
-                "https://houston.example.com/v1/authorization"
-                in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
-            )
-
-    def test_ingress_with_internal_authorization(self, kube_version):
-        """Test Alertmanager Service with authSidecar."""
-        docs = render_chart(
-            kube_version=kube_version,
-            values={},
-            show_only=[
-                "charts/alertmanager/templates/ingress.yaml",
-                "charts/prometheus/templates/ingress.yaml",
-            ],
-        )
-
-        assert len(docs) == 2
-        for doc in docs:
-            assert doc["kind"] == "Ingress"
-            assert doc["apiVersion"] == "networking.k8s.io/v1"
-            assert (
                 "http://release-name-houston.default.svc.cluster.local:8871/v1/authorization"
                 in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
             )
