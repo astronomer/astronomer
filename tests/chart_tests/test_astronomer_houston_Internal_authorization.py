87af10e3326e0cd647dcf6a1592a45efc29dc052
pgvishnuram <vishnu@astronomer.io>
2025-09-13 05:21:18 +0530

    update new tests

--- tests/chart_tests/test_astronomer_houston_Internal_authorization.py
+++ tests/chart_tests/test_astronomer_houston_Internal_authorization.py
@@ -28,3 +28,23 @@ class TestHoustonInternalAuthorization:
                 "http://release-name-houston.default.svc.cluster.local:8871/v1/authorization"
                 in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
             )
+
+    def test_ingress_with_authorization_with_dataplane_mode(self, kube_version):
+        """Test Alertmanager Service with authSidecar."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"plane": {"mode": "data"}}},
+            show_only=[
+                "charts/alertmanager/templates/ingress.yaml",
+                "charts/prometheus/templates/ingress.yaml",
+            ],
+        )
+
+        assert len(docs) == 2
+        for doc in docs:
+            assert doc["kind"] == "Ingress"
+            assert doc["apiVersion"] == "networking.k8s.io/v1"
+            assert (
+                "https://houston.example.com/v1/authorization"
+                in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
+            )
