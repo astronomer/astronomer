711305e8227442d87c946e728297631ef74197f1
pgvishnuram <81585115+pgvishnuram@users.noreply.github.com>
2025-09-13 06:17:41 +0530

    remove houston internal auth flag (#2835)
    Co-authored-by: Rishabh Karajgi <rishabh.karajgi@gmail.com>

--- tests/chart_tests/test_astronomer_houston_internal_authorization.py
+++ tests/chart_tests/test_astronomer_houston_internal_authorization.py
@@ -25,26 +25,45 @@ class TestHoustonInternalAuthorization:
             assert doc["kind"] == "Ingress"
             assert doc["apiVersion"] == "networking.k8s.io/v1"
             assert (
-                "https://houston.example.com/v1/authorization"
+                "http://release-name-houston.default.svc.cluster.local:8871/v1/authorization"
                 in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
             )
 
-    def test_ingress_with_internal_authorization(self, kube_version):
+    def test_ingress_with_authorization_with_control_mode(self, kube_version):
         """Test Alertmanager Service with authSidecar."""
         docs = render_chart(
             kube_version=kube_version,
-            values={"global": {"enableHoustonInternalAuthorization": True}},
+            values={"global": {"plane": {"mode": "control"}}},
             show_only=[
                 "charts/alertmanager/templates/ingress.yaml",
                 "charts/prometheus/templates/ingress.yaml",
             ],
         )
-
         assert len(docs) == 2
         for doc in docs:
             assert doc["kind"] == "Ingress"
             assert doc["apiVersion"] == "networking.k8s.io/v1"
             assert (
                 "http://release-name-houston.default.svc.cluster.local:8871/v1/authorization"
+                in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
+            )
+
+    def test_ingress_with_authorization_with_dataplane_mode(self, kube_version):
+        """Test Alertmanager Service with authSidecar."""
+        docs = render_chart(
+            kube_version=kube_version,
+            values={"global": {"plane": {"mode": "data"}}},
+            show_only=[
+                "charts/alertmanager/templates/ingress.yaml",
+                "charts/prometheus/templates/ingress.yaml",
+            ],
+        )
+
+        assert len(docs) == 1
+        for doc in docs:
+            assert doc["kind"] == "Ingress"
+            assert doc["apiVersion"] == "networking.k8s.io/v1"
+            assert (
+                "https://houston.example.com/v1/authorization"
                 in doc["metadata"]["annotations"]["nginx.ingress.kubernetes.io/auth-url"]
             )
