ebc2999e8fc0760fa0fda865d9a01ef04ac81490
Shubham-astro <shubham.singh@astronomer.io>
2025-09-26 20:44:56 +0530

    correcting camelcase

--- tests/chart_tests/test_byo_sa.py
+++ tests/chart_tests/test_byo_sa.py
@@ -330,7 +330,7 @@ custom_service_account_names = {
         "external-es-proxy": {"serviceAccount": {"create": True, "name": "prothean"}}
     },
     "charts/nats/templates/jetstream-job.yaml": {
-        "nats": {"nats": {"jetstream": {"serviceAccount": {"create": True, "name": "prothean"}}}}
+        "nats": {"nats": {"jetStream": {"serviceAccount": {"create": True, "name": "prothean"}}}}
     },
     "charts/nats/templates/statefulset.yaml": {"nats": {"nats": {"serviceAccount": {"create": True, "name": "prothean"}}}},
     "charts/nginx/templates/nginx-deployment-default.yaml": {
