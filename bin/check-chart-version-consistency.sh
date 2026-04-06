3fd787a70f1073b0972d8558691ef55980b8c65a
Shubham-astro <shubham.singh@astronomer.io>
2026-04-06 15:30:15 +0530

    add exe script for checking chart consistency

---
+++ bin/check-chart-version-consistency.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+unique_versions=$(awk '/^(version|appVersion)/ {print $2}' Chart.yaml charts/astronomer/Chart.yaml | sort -u | wc -l)
+
+if [[ "$unique_versions" -ne 1 ]]; then
+  echo "ERROR: version/appVersion mismatch between Chart.yaml and charts/astronomer/Chart.yaml"
+  awk '/^(version|appVersion)/ {print FILENAME": "$0}' Chart.yaml charts/astronomer/Chart.yaml
+  exit 1
+fi
+
+echo "Chart versions are consistent"
