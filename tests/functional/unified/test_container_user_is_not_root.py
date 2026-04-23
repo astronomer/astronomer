c9990984a53dad3480599d8d00a07a6a88752c3a
Daniel Hoherd <daniel.hoherd@gmail.com>
2026-04-23 16:44:23 -0400

    Try to parametrize root user test

--- tests/functional/unified/test_container_user_is_not_root.py
+++ tests/functional/unified/test_container_user_is_not_root.py
@@ -5,20 +5,19 @@ from tests.utils.k8s import KUBECONFIG_UNIFIED, get_pod_running_containers
 
 container_ignore_list = ["kube-state", "houston", "astro-ui"]
 
+_all_containers = get_pod_running_containers(kubeconfig=KUBECONFIG_UNIFIED, namespace="astronomer")
+_test_containers = {k: v for k, v in _all_containers.items() if v["_name"] not in container_ignore_list}
 
-def test_container_user_is_not_root():
-    container_list = get_pod_running_containers(kubeconfig=KUBECONFIG_UNIFIED, namespace="astronomer")
-    for container in container_list.values():
-        if container["_name"] in container_ignore_list:
-            pytest.skip("Info: Unsupported container: " + container["_name"])
 
-        pod_client = testinfra.get_host(
-            f"kubectl://{container['pod_name']}?container={container['_name']}&namespace={container['namespace']}",
-            kubeconfig=KUBECONFIG_UNIFIED,
-        )
+@pytest.mark.parametrize("container", _test_containers.values(), ids=_test_containers.keys())
+def test_container_user_is_not_root(container):
+    pod_client = testinfra.get_host(
+        f"kubectl://{container['pod_name']}?container={container['_name']}&namespace={container['namespace']}",
+        kubeconfig=KUBECONFIG_UNIFIED,
+    )
 
-        user_info = pod_client.user()
-        assert user_info.name != "root"
-        assert user_info.group != "root"
-        assert user_info.gid != 0
-        assert user_info.uid != 0
+    user_info = pod_client.user()
+    assert user_info.name != "root"
+    assert user_info.group != "root"
+    assert user_info.gid != 0
+    assert user_info.uid != 0
