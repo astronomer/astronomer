ffe584af99170dec6a0a940ad2fd08833e77f6e3
pgvishnuram <vishnu@astronomer.io>
2026-08-26 16:40:41 +0530

    remove extraAnnotation commander meta schema

--- tests/chart_tests/test_astronomer_commander.py
+++ tests/chart_tests/test_astronomer_commander.py
@@ -93,30 +93,6 @@ class TestAstronomerCommander:
             "openshift": {"enabled": False},
         }
 
-    def test_commander_metadata_extra_annotations(self, kube_version):
-        """Test that global.extraAnnotations are passed through to commander metadata.yaml."""
-        extra_annotations = {
-            "kubernetes.io/ingress.class": "default",
-            "route.openshift.io/termination": "passthrough",
-        }
-        values = {
-            "global": {
-                "extraAnnotations": extra_annotations,
-            },
-            "astronomer": {
-                "images": {"registry": {"tag": "99.88.77"}},
-            },
-        }
-        docs = render_chart(
-            kube_version=kube_version,
-            values=values,
-            show_only=["charts/astronomer/templates/commander/commander-metadata.yaml"],
-        )
-
-        assert len(docs) == 1
-        metadata_file_contents = yaml.safe_load(docs[0]["data"]["metadata.yaml"])
-        assert metadata_file_contents["extraAnnotations"] == extra_annotations
-
     def test_commander_metadata_shared_elasticsearch_overrides(self, kube_version):
         """Test that global.sharedElasticsearch are passed through to commander metadata.yaml."""
         values = {
