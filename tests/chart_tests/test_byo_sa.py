bcf2c76e301828ddad2f36f3375d5db276ea996b
pgvishnuram <vishnu@astronomer.io>
2026-09-11 15:20:19 +0530

    cleanup chart values

--- tests/chart_tests/test_byo_sa.py
+++ tests/chart_tests/test_byo_sa.py
@@ -192,6 +192,7 @@ class TestServiceAccounts:
                 "nodeExporter": {"enabled": True},
                 "pgbouncer": {"enabled": True},
                 "airflowOperator": {"enabled": True},
+                "laminar": {"enabled": True},
             },
             "astronomer": {
                 "commander": {"serviceAccount": {"create": False}},
@@ -206,6 +207,8 @@ class TestServiceAccounts:
             "alertmanager": {"serviceAccount": {"create": False}},
             "postgresql": {"serviceAccount": {"create": False}},
             "external-es-proxy": {"serviceAccount": {"create": False}},
+            "laminar": {"serviceAccount": {"create": False},
+                        "databaseBootstrapper": {"serviceAccount": {"create": False}}},
             "prometheus-postgres-exporter": {"serviceAccount": {"create": False}},
             "prometheus-node-exporter": {"serviceAccount": {"create": False}},
             "pgbouncer": {"serviceAccount": {"create": False}},
@@ -244,6 +247,7 @@ class TestServiceAccounts:
                 "nodeExporter": {"enabled": True},
                 "pgbouncer": {"enabled": True},
                 "airflowOperator": {"enabled": True},
+                "laminar": {"enabled": True},
             },
             "astronomer": {
                 "commander": {"serviceAccount": {"create": True, "annotations": annotations}},
@@ -275,6 +279,8 @@ class TestServiceAccounts:
                 "serviceAccount": {"create": True, "annotations": annotations},
                 "webhook": {"serviceAccount": {"create": True, "annotations": annotations}},
             },
+            "laminar": {"serviceAccount": {"create": False},
+                                    "databaseBootstrapper": {"serviceAccount": {"create": False}}},
         }
         show_only = [
             str(path.relative_to(git_root_dir)) for path in git_root_dir.rglob("charts/**/*") if "serviceaccount" in str(path)
